572533c97847c72147a739780a3f00fa9d9f8fe2
ogous <oguzxan.ali@gmail.com>
2024-09-05 19:23:12 +0300

    style: add button to cta comp

--- web/src/components/detail/detailAction.tsx
+++ web/src/components/detail/detailAction.tsx
@@ -1,7 +1,10 @@
+import Button from "@/components/themed/button";
+
 export default function DetailCall2Action() {
   return (
     <div className="rounded-md border border-black/10 shadow-md">
       <h2>Kamala Haris</h2>
+      <Button title="Buy" />
     </div>
   );
 }
