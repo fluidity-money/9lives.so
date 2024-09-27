3a359ab2ac645d9724edefe1d1e1106d2297dcc4
ogous <oguzxan.ali@gmail.com>
2024-09-27 15:14:42 +0300

    refactor: gtm with conset config

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { ThirdwebProvider } from "thirdweb/react";
 import ReactQueryProvider from "./reactQuery";
+import ContextInjector from "./contextInjector";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThirdwebProvider>
-      <ReactQueryProvider>{children}</ReactQueryProvider>
+      <ReactQueryProvider>
+        <ContextInjector />
+        {children}
+      </ReactQueryProvider>
     </ThirdwebProvider>
   );
 }
