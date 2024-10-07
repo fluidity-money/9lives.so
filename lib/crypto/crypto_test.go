f6b225c4e129962c7be9968321503b06da4f1894
bayge <alex@fluidity.money>
2024-10-07 18:38:49 +1030

    Reproduce online trading address creation

--- lib/crypto/crypto_test.go
+++ lib/crypto/crypto_test.go
@@ -10,31 +10,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestContractBytecode based on the Solidity tests.
-var TestContractBytecode, _ = hex.DecodeString("602d5f8160095f39f35f5f365f5f37365f7300000000000000000000000000000000000000005af43d5f5f3e6029573d5ffd5b3d5ff3")
+var (
+	// TestContractBytecode based on the Solidity tests.
+	TestContractBytecode, _ = hex.DecodeString("602d5f8160095f39f35f5f365f5f37365f7300000000000000000000000000000000000000005af43d5f5f3e6029573d5ffd5b3d5ff3")
 
-var TestContractBytecodeHashed = ethCrypto.Keccak256(TestContractBytecode)
+	// TestContractBytecodeRealWorld based on the real world deployment on testnet (maybe old).
+	TestContractBytecodeRealWorld, _ = hex.DecodeString("602d5f8160095f39f35f5f365f5f37365f73c5f7a07a0fdfbe00f85dca1f088d8cdc9ef559b75af43d5f5f3e6029573d5ffd5b3d5ff3")
+)
+
+var (
+	TestContractBytecodeHashed          = ethCrypto.Keccak256(TestContractBytecode)
+	TestContractBytecodeRealWorldHashed = ethCrypto.Keccak256(TestContractBytecodeRealWorld)
+)
 
 var testGetTradingAddress = []struct {
-	outcomes         []Outcome
-	expectedIds      []string
-	factoryAddress   string
+	outcomes             []Outcome
+	expectedIds          []string
+	factoryAddress       string
 	contractBytecodeHash []byte
 
 	tradingAddr string
-}{{
-	[]Outcome{{"Koko", "Cat", 0}, {"Leo", "Dog", 123}},
-	[]string{
-		//bytes8(keccak256(abi.encodePacked("Leo", "Dog", uint8(0))))
-		"3b79565a915eb950",
-		//bytes8(keccak256(abi.encodePacked("Koko", "Cat", uint8(0))))
-		"8f885992cafd4d5c",
-	},
-	"0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496",
-	TestContractBytecodeHashed,
+}{
+	{
+		[]Outcome{{"Koko", "Cat", 0}, {"Leo", "Dog", 123}},
+		[]string{
+			//bytes8(keccak256(abi.encodePacked("Leo", "Dog", uint8(123))))
+			"3b79565a915eb950",
+			//bytes8(keccak256(abi.encodePacked("Koko", "Cat", uint8(0))))
+			"8f885992cafd4d5c",
+		},
+		"0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496",
+		TestContractBytecodeHashed,
 
-	"0xDA7EaC0f112e8ED91c951972c60f8147FE51bbD4",
-}}
+		"0xDA7EaC0f112e8ED91c951972c60f8147FE51bbD4",
+	},
+	{
+		[]Outcome{{"Koko", "Cat", 0}, {"Leo", "Dog", 123}},
+		[]string{
+			//bytes8(keccak256(abi.encodePacked("Leo", "Dog", uint8(123))))
+			"3b79565a915eb950",
+			//bytes8(keccak256(abi.encodePacked("Koko", "Cat", uint8(0))))
+			"8f885992cafd4d5c",
+		},
+		"0xa088e2bc674e98ac3f2b251ca4fece66bdecb50b",
+		TestContractBytecodeRealWorldHashed,
+
+		"0x30882f33dde2d01c5483f1cdb2533ef019b71e27",
+	},
+}
 
 func TestGetTradingAddr(t *testing.T) {
 	for i, test := range testGetTradingAddress {
