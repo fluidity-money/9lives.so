f5772b907d4891ebca7459cb3aee956f59faace1
ogous <oguzxan.ali@gmail.com>
2024-10-08 19:06:25 +0300

    feat: add get share contract address func

--- cmd/graphql.ethereum/graph/ethClient.go
+++ cmd/graphql.ethereum/graph/ethClient.go
@@ -51,3 +51,43 @@ func isContractCreated(c *ethclient.Client, factoryAdd ethCommon.Address, tradin
 	}
 	return &wasCreated, nil
 }
+
+func getShareAddr(c *ethclient.Client, tradingAddr ethCommon.Address, outcomeId string) (*ethCommon.Address, error) {
+	abiJSON := `[{"inputs":[{"internalType":"bytes8","name":"outcome","type":"bytes8"}],"name":"shareAddr","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]`
+	parsedABI, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		slog.Error("factory abi could not be parsed",
+			"outcome id", outcomeId,
+			"to be verified trading address", tradingAddr,
+			"error", err,
+		)
+		return nil, fmt.Errorf("factory abi could not be parsed")
+	}
+	callData, err := parsedABI.Pack("shareAddr", outcomeId)
+	if err != nil {
+		slog.Error("callData could not be prepared",
+			"outcome id", outcomeId,
+			"to be verified trading address", tradingAddr,
+			"error", err,
+		)
+		return nil, fmt.Errorf("callData could not be prepared")
+	}
+	result, err := c.CallContract(context.Background(), ethereum.CallMsg{
+		To:   &tradingAddr,
+		Data: callData,
+	}, nil)
+	if err != nil {
+		slog.Error("can not call wasCreated from contract",
+			"trading address", tradingAddr,
+			"call data", callData,
+			"error", err,
+		)
+		return nil, fmt.Errorf("can not call shareAddr from contract")
+	}
+	var shareAddr ethCommon.Address
+	err = parsedABI.UnpackIntoInterface(&shareAddr, "shareAddr", result)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unpack contract call result")
+	}
+	return &shareAddr, nil
+}
