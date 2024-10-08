fd30a4de25c7d1490610e63d75f0bcebe0fd8855
ogous <oguzxan.ali@gmail.com>
2024-10-08 22:12:41 +0300

    fix: migration

--- db/migrations/1726296116-init_frontpage.sql
+++ db/migrations/1726296116-init_frontpage.sql
@@ -1,20 +1,5 @@
 -- migrate:up
 
--- Frontpage state deserialised, and displayed literally on the landing page.
-
-CREATE TABLE frontpage (
-	id SERIAL PRIMARY KEY,
-	created_by TIMESTAMP NOT NULL,
-	updated_by TIMESTAMP NOT NULL,
-	until TIMESTAMP NOT NULL,
-	campaign_id TEXT
-	CONSTRAINT fk_campaign FOREIGN KEY (campaign_id) REFERENCES campaigns_1(id)
-);
-
-CREATE INDEX ON frontpage (created_by);
-
-CREATE INDEX ON frontpage (until);
-
 -- Campaigns that were created, and are accessible somehow.
 
 CREATE TABLE campaigns_1 (
@@ -23,5 +8,20 @@ CREATE TABLE campaigns_1 (
 	updated_by TIMESTAMP NOT NULL,
 	content JSONB NOT NULL
 );
+
+-- Frontpage state deserialised, and displayed literally on the landing page.
+
+CREATE TABLE frontpage (
+	id SERIAL PRIMARY KEY,
+	created_by TIMESTAMP NOT NULL,
+	updated_by TIMESTAMP NOT NULL,
+	until TIMESTAMP NOT NULL,
+	campaign_id TEXT NOT NULL,
+	CONSTRAINT fk_campaign FOREIGN KEY (campaign_id) REFERENCES campaigns_1(id)
+);
+
+CREATE INDEX ON frontpage (created_by);
+
+CREATE INDEX ON frontpage (until);
 
 -- migrate:down
