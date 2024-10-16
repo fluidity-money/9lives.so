f6c939940fec13052212f039f7d4d4835d21407d
ogous <oguzxan.ali@gmail.com>
2024-10-16 16:48:49 +0300

    fix: manage setInterval

--- web/src/components/clock.tsx
+++ web/src/components/clock.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Clock() {
   const [date, setDate] = useState<Date>(new Date());
+  useEffect(() => {
+   const timer = setInterval(() => setDate(new Date()), 1000);
+  
+   return () => {
+    clearInterval(timer);
+  };
+  }, [])
 
-  setInterval(() => setDate(new Date()), 1000);
   return (
     <div className="flex h-10 items-center justify-center border-l-2 border-l-black px-4 font-chicago">
       {date.toLocaleTimeString("en", {
