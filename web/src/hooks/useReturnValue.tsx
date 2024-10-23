a25528c3e875316046c6d363d9426e9a2c7cf945
ogous <oguzxan.ali@gmail.com>
2024-10-23 14:48:09 +0300

    refactor: add guard for unnecesary refetches

--- web/src/hooks/useReturnValue.tsx
+++ web/src/hooks/useReturnValue.tsx
@@ -10,6 +10,7 @@ import { toUnits } from "thirdweb/utils";
 import { formatUnits, MaxUint256 } from "ethers";
 import { Account } from "thirdweb/wallets";
 import { useQuery } from "@tanstack/react-query";
+import { useCallback, useMemo } from "react";
 
 export default function useReturnValue({
   account,
@@ -27,37 +28,58 @@ export default function useReturnValue({
   share: number;
 }) {
   const amount = toUnits(share.toString(), config.contracts.decimals.fusdc);
-  const checkAmmReturnTx = prepareContractCall({
-    contract: config.contracts.lens,
-    method: "getLongtailQuote",
-    params: [shareAddr, true, amount, MaxUint256],
-  });
-  const tradingContract = getContract({
-    abi: tradingAbi,
-    address: tradingAddr,
-    client: thirdwebClient,
-    chain: config.chains.superpositionTestnet,
-  });
-  const check9liveReturnTx = (receipent: string) =>
-    prepareContractCall({
-      contract: tradingContract,
-      method: "quote101CBE35",
-      params: [outcomeId, amount, receipent],
-    });
-  async function getReturns(account: Account) {
-    return await Promise.all<bigint>([
-      simulateTransaction({
-        transaction: checkAmmReturnTx,
-        account,
+  const checkAmmReturnTx = useMemo(
+    () =>
+      prepareContractCall({
+        contract: config.contracts.lens,
+        method: "getLongtailQuote",
+        params: [shareAddr, true, amount, MaxUint256],
       }),
-      simulateTransaction({
-        transaction: check9liveReturnTx(account.address),
-        account,
+    [amount, shareAddr],
+  );
+  const tradingContract = useMemo(
+    () =>
+      getContract({
+        abi: tradingAbi,
+        address: tradingAddr,
+        client: thirdwebClient,
+        chain: config.chains.superpositionTestnet,
       }),
-    ]);
-  }
+    [tradingAddr],
+  );
+  const check9liveReturnTx = useCallback(
+    (receipent: string) =>
+      prepareContractCall({
+        contract: tradingContract,
+        method: "quote101CBE35",
+        params: [outcomeId, amount, receipent],
+      }),
+    [amount, outcomeId, tradingContract],
+  );
+  const getReturns = useCallback(
+    async function (account: Account) {
+      return await Promise.all<bigint>([
+        simulateTransaction({
+          transaction: checkAmmReturnTx,
+          account,
+        }),
+        simulateTransaction({
+          transaction: check9liveReturnTx(account.address),
+          account,
+        }),
+      ]);
+    },
+    [check9liveReturnTx, checkAmmReturnTx],
+  );
   return useQuery({
-    queryKey: ["returnValue", shareAddr, tradingAddr, outcomeId, share],
+    queryKey: [
+      "returnValue",
+      shareAddr,
+      tradingAddr,
+      outcomeId,
+      share,
+      account,
+    ],
     queryFn: async () => {
       const [returnAmm, return9lives] = await getReturns(account!);
       const returnValue =
--- web/src/hooks/useSharePrices.tsx
+++ web/src/hooks/useSharePrices.tsx
@@ -8,6 +8,7 @@ import {
 } from "thirdweb";
 import { formatUnits } from "ethers";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 export default function useSharePrices({
   tradingAddr,
@@ -16,25 +17,33 @@ export default function useSharePrices({
   tradingAddr: string;
   outcomeIds: `0x${string}`[];
 }) {
-  const tradingContract = getContract({
-    abi: tradingAbi,
-    address: tradingAddr,
-    client: thirdwebClient,
-    chain: config.chains.superpositionTestnet,
-  });
-
-  const promises = outcomeIds.map((outcomeId) =>
-    simulateTransaction({
-      transaction: prepareContractCall({
-        contract: tradingContract,
-        method: "priceF3C364BC",
-        params: [outcomeId],
+  const tradingContract = useMemo(
+    () =>
+      getContract({
+        abi: tradingAbi,
+        address: tradingAddr,
+        client: thirdwebClient,
+        chain: config.chains.superpositionTestnet,
       }),
-    }),
+    [tradingAddr],
+  );
+
+  const promises = useMemo(
+    () =>
+      outcomeIds.map((outcomeId) =>
+        simulateTransaction({
+          transaction: prepareContractCall({
+            contract: tradingContract,
+            method: "priceF3C364BC",
+            params: [outcomeId],
+          }),
+        }),
+      ),
+    [outcomeIds, tradingContract],
   );
 
   return useQuery<{ id: `0x${string}`; price: string }[]>({
-    queryKey: ["sharePrices", tradingAddr],
+    queryKey: ["sharePrices", tradingAddr, promises],
     queryFn: async () => {
       const res = await Promise.all<bigint>(promises);
       return res.map((price, idx) => ({
