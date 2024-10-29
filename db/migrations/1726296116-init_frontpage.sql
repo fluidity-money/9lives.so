a52a0a4dc2bae3b37a46a25d984f29330b139fbb
bayge <alex@fluidity.money>
2024-10-29 15:35:09 +1030

    Update categories db to use consistent campaign id

--- db/migrations/1726296116-init_frontpage.sql
+++ db/migrations/1726296116-init_frontpage.sql
@@ -14,7 +14,7 @@ CREATE TABLE ninelives_frontpage_1 (
 	"from" TIMESTAMP NOT NULL,
 	until TIMESTAMP NOT NULL,
 	campaign_id TEXT NOT NULL,
-	CONSTRAINT fk_campaign FOREIGN KEY (campaign_id) REFERENCES ninelives_campaigns_1(id)
+	FOREIGN KEY (campaign_id) REFERENCES ninelives_campaigns_1(id)
 );
 
 CREATE TABLE ninelives_categories_1 (
@@ -25,11 +25,11 @@ CREATE TABLE ninelives_categories_1 (
 );
 
 CREATE TABLE ninelives_campaigns_categories_1 (
-	campaign_id INT NOT NULL,
+	campaign_id TEXT NOT NULL,
 	category_id INT NOT NULL,
 	PRIMARY KEY (campaign_id, category_id),
-	CONSTRAINT fk_campaign FOREIGN KEY (campaign_id) REFERENCES ninelives_campaigns_1(id),
-	CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES ninelives_categories_1(id)
+	FOREIGN KEY (campaign_id) REFERENCES ninelives_campaigns_1(id),
+	FOREIGN KEY (category_id) REFERENCES ninelives_categories_1(id)
 );
 
 CREATE INDEX ON ninelives_frontpage_1 ("from");
