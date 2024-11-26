5511f4aaae3d1a98401dbbd6b0bc36e11cc169ab
ogous <oguzxan.ali@gmail.com>
2024-11-26 15:35:57 +0300

    chore: add keccak lib

---
+++ web/src/utils/generateId.ts
@@ -0,0 +1,25 @@
+import keccak from "keccak";
+
+export function generateId(name: string, desc: string, seed: number) {
+  try {
+    // Ensure the seed is a valid uint8 value
+    if (
+      typeof seed !== "number" ||
+      seed < 0 ||
+      seed > 255 ||
+      !Number.isInteger(seed)
+    ) {
+      throw new Error("Seed must be an 8-bit unsigned integer (uint8).");
+    }
+    const nameBuffer = Buffer.from(name, "utf-8");
+    const descBuffer = Buffer.from(desc, "utf-8");
+    const seedBuffer = Buffer.alloc(1);
+    seedBuffer.writeUInt8(seed); // Write seed as uint8
+    const combinedBuffer = Buffer.concat([nameBuffer, descBuffer, seedBuffer]);
+    const hash = keccak("keccak256").update(combinedBuffer).digest();
+    return `0x${hash.subarray(0, 8).toString("hex")}`;
+  } catch (err) {
+    console.error("Error generating outcome ID:", err);
+    throw err;
+  }
+}
