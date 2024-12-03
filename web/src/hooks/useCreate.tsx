984bf032e588a2b6d87ee4e15939f515e89ee934
ogous <oguzxan.ali@gmail.com>
2024-12-03 18:17:08 +0300

    add retry logic to create campaign

--- web/src/hooks/useCreate.tsx
+++ web/src/hooks/useCreate.tsx
@@ -5,7 +5,7 @@ import { keccak256 } from "ethers";
 import { Account } from "thirdweb/wallets";
 import { useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { CampaignInput, OutcomeInput } from "@/types";
+import { CampaignInput } from "@/types";
 import { useRouter } from "next/router";
 import { requestCreateCampaign } from "@/providers/graphqlClient";
 import { useCampaignStore } from "@/stores/campaignStore";
@@ -14,43 +14,37 @@ const useCreate = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
   const upsertCampaign = useCampaignStore((s) => s.upsertCampaign);
-  const create = async (
-    input: CampaignInput,
-    account: Account,
-    amounts: number[],
-    outcomes: OutcomeInput[],
-    urlCommittee: string,
-  ) =>
+  const draftCampaigns = useCampaignStore((s) => s.campaigns);
+  const create = async (input: CampaignInput, account: Account) =>
     toast.promise(
       new Promise(async (res, rej) => {
         try {
-          const creationList = outcomes.map((o, idx) => ({
-            identifier: o.id!,
-            amount: toUnits(
-              amounts[idx].toString(),
-              config.contracts.decimals.fusdc,
-            ),
-          }));
-          const hashedUrl = keccak256(urlCommittee) as `0x${string}`;
-          const createTx = prepareContractCall({
-            contract: config.contracts.factory,
-            method: "newTradingC11AAA3B",
-            params: [
-              creationList,
-              config.NEXT_PUBLIC_INFRA_ORACLE_ADDR,
-              BigInt(0),
-              BigInt(0),
-              hashedUrl,
-              account.address,
-            ],
-          });
-          await sendTransaction({
-            transaction: createTx,
-            account,
-          });
-          // upsert campaign if on-chain campaign is created
-          // so backend creation can be retried again
-          upsertCampaign(input);
+          const draftCampaign = draftCampaigns.find((c) => c.id === input.id);
+          if (!draftCampaign) {
+            const creationList = input.outcomes.map((o) => ({
+              identifier: o.id!,
+              sqrtPrice: BigInt(79228162514264337593543950336), // with $1 for each outcome
+              name: o.name,
+            }));
+            const hashedUrl = keccak256(input.urlCommitee) as `0x${string}`;
+            const createTx = prepareContractCall({
+              contract: config.contracts.helper,
+              method: "createWithInfraMarket",
+              params: [
+                creationList, // outcomes
+                BigInt(0), // time ending
+                hashedUrl, // documentation url committee
+                account.address, // fee recipient
+              ],
+            });
+            await sendTransaction({
+              transaction: createTx,
+              account,
+            });
+            // upsert campaign if on-chain campaign is created
+            // so backend creation can be retried again
+            upsertCampaign(input);
+          }
           await requestCreateCampaign({
             ...input,
             starting: new Date(input.starting).getTime(),
