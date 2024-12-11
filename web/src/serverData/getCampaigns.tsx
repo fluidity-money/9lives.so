a37d90b3e4fefd123d93e8d7c1d56ae628282a1b
ogous <oguzxan.ali@gmail.com>
2024-12-11 15:11:01 +0300

    use custom fetch for ISR of campaign details

--- web/src/serverData/getCampaigns.tsx
+++ web/src/serverData/getCampaigns.tsx
@@ -1,10 +1,48 @@
-import { requestCampaignList } from "@/providers/graphqlClient";
+// import { requestCampaignList } from "@/providers/graphqlClient";
 import { unstable_cache } from "next/cache";
 import { Campaign } from "@/types";
+import appConfig from "@/config";
+const query = `
+    query Campaigns {
+      campaigns {
+        name
+        identifier
+        description
+        picture
+        oracle
+        poolAddress
+        creator {
+          address
+        }
+        outcomes {
+          identifier
+          name
+          description
+          picture
+          share {
+            address
+          }
+        }
+        ending
+        starting
+      }
+    }
+  `;
 
 export async function getCampaigns() {
-  const campaigns = (await requestCampaignList) as Campaign[];
-  // const campaigns = res as Campaign[];
+  const res = await fetch(appConfig.NEXT_PUBLIC_GRAPHQL_URL, {
+    method: "POST",
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+    body: JSON.stringify({ query }),
+  });
+  const result = await res.json();
+  const campaigns = result.data.campaigns as Campaign[];
   return campaigns.map((campaign) => {
     campaign["isYesNo"] =
       campaign.outcomes.length === 2 &&
