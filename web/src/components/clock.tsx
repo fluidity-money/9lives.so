0438928be5d263866b8a02bf902944be7a19f106
ogous <oguzxan.ali@gmail.com>
2024-12-11 15:11:54 +0300

    suppressHydrationWarning for clock time

--- web/src/components/clock.tsx
+++ web/src/components/clock.tsx
@@ -13,7 +13,10 @@ export default function Clock() {
   }, []);
 
   return (
-    <div className="flex h-10 items-center justify-center border-l-2 border-l-black px-4 font-chicago">
+    <div
+      className="flex h-10 items-center justify-center border-l-2 border-l-black px-4 font-chicago"
+      suppressHydrationWarning
+    >
       {date.toLocaleTimeString("en", {
         year: undefined,
         month: "long",
