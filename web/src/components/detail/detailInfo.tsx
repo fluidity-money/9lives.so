04fa6aabd39f7eb3d98fa579cf1dfbf81f0b7fd5
ogous <oguzxan.ali@gmail.com>
2024-12-16 21:14:53 +0300

    style: improve detail info

--- web/src/components/detail/detailInfo.tsx
+++ web/src/components/detail/detailInfo.tsx
@@ -26,30 +26,38 @@ export default function DetailInfo({
   };
   return (
     <>
-      <RetroCard title="Rules & Resources" showClose={false}>
-        <h5 className="mb-5 font-chicago text-sm">Overview</h5>
-        <p className="text-xs">{desc}</p>
-        <br />
-        <h5 className="mb-5 font-chicago text-sm">Settlement</h5>
-        <p className="text-xs">
-          Settlement of this trade is completed with{" "}
-          {settlementDescMap[settlement]}.
-        </p>
-        {oracleDescription && settlement === "ORACLE" ? (
+      <RetroCard
+        title="Rules & Resources"
+        showClose={false}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-2.5">
+          <h5 className="font-chicago text-sm">Overview</h5>
+          <p className="text-xs">{desc}</p>
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <h5 className="font-chicago text-sm">Settlement</h5>
           <p className="text-xs">
-            This market concludes at: <br />
-            {oracleDescription}
+            Settlement of this trade is completed with{" "}
+            {settlementDescMap[settlement] ?? settlementDescMap["ORACLE"]}.
           </p>
-        ) : null}
-        <h6 className="text-xs">Settlement source: </h6>
-        <Link
-          className="text-xs underline"
-          rel="noopener noreferrer"
-          target="_blank"
-          href={`https://testnet-explorer.superposition.so/address/${settlementContractMap[settlement]}`}
-        >
-          {settlementContractMap[settlement]}
-        </Link>
+          {oracleDescription && settlement === "ORACLE" ? (
+            <p className="text-xs">
+              This market concludes at: <br />
+              {oracleDescription}
+            </p>
+          ) : null}
+          <h6 className="text-xs">Settlement source: </h6>
+          <Link
+            className="text-xs underline"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`https://testnet-explorer.superposition.so/address/${settlementContractMap[settlement] ?? settlementContractMap["ORACLE"]}`}
+          >
+            {settlementContractMap[settlement] ??
+              settlementContractMap["ORACLE"]}
+          </Link>
+        </div>
       </RetroCard>
       <RetroCard title="Creator" showClose={false}>
         <h5 className="mb-5 font-chicago text-sm">Creator</h5>
@@ -58,6 +66,7 @@ export default function DetailInfo({
           <Link
             rel="noopener noreferrer"
             target="_blank"
+            className="text-xs underline"
             href={`https://testnet-explorer.superposition.so/address/${creator}`}
           >
             {creator}
