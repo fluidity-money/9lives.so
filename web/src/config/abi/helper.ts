f71c6cfc7583bcedcf13b24fd8e38ec8b68c0238
ogous <oguzxan.ali@gmail.com>
2024-12-18 15:19:56 +0300

    chore: udpate abi

--- web/src/config/abi/helper.ts
+++ web/src/config/abi/helper.ts
@@ -276,6 +276,138 @@ const helperAbi = [
   },
   {
     type: "function",
+    name: "createWithCustom",
+    inputs: [
+      {
+        name: "oracle",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "outcomes",
+        type: "tuple[]",
+        internalType: "struct FactoryOutcome[]",
+        components: [
+          {
+            name: "identifier",
+            type: "bytes8",
+            internalType: "bytes8",
+          },
+          {
+            name: "sqrtPrice",
+            type: "uint256",
+            internalType: "uint256",
+          },
+          {
+            name: "name",
+            type: "string",
+            internalType: "string",
+          },
+        ],
+      },
+      {
+        name: "timeEnding",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "documentation",
+        type: "bytes32",
+        internalType: "bytes32",
+      },
+      {
+        name: "feeRecipient",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "tradingAddr",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "createWithCustomPermit",
+    inputs: [
+      {
+        name: "oracle",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "outcomes",
+        type: "tuple[]",
+        internalType: "struct FactoryOutcome[]",
+        components: [
+          {
+            name: "identifier",
+            type: "bytes8",
+            internalType: "bytes8",
+          },
+          {
+            name: "sqrtPrice",
+            type: "uint256",
+            internalType: "uint256",
+          },
+          {
+            name: "name",
+            type: "string",
+            internalType: "string",
+          },
+        ],
+      },
+      {
+        name: "timeEnding",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "documentation",
+        type: "bytes32",
+        internalType: "bytes32",
+      },
+      {
+        name: "feeRecipient",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "deadline",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "v",
+        type: "uint8",
+        internalType: "uint8",
+      },
+      {
+        name: "r",
+        type: "bytes32",
+        internalType: "bytes32",
+      },
+      {
+        name: "s",
+        type: "bytes32",
+        internalType: "bytes32",
+      },
+    ],
+    outputs: [
+      {
+        name: "tradingAddr",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
     name: "createWithInfraMarket",
     inputs: [
       {
