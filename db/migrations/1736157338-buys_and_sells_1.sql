917c6d56c873336136a87410534eac4fda5b8a26
ogous <oguzxan.ali@gmail.com>
2025-01-06 22:22:26 +0300

    feat: add buys and sells table version 1

---
+++ db/migrations/1736157338-buys_and_sells_1.sql
@@ -0,0 +1,72 @@
+-- migrate:up
+CREATE TABLE ninelives_buys_and_sells_1 (
+    transaction_hash HASH PRIMARY KEY,
+    created_by TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    block_hash HASH NOT NULL,
+    block_number INTEGER NOT NULL,
+    emitter_addr ADDRESS NOT NULL,
+    from_amount HUGEINT NOT NULL,
+    from_symbol VARCHAR(30) NOT NULL,
+    to_amount HUGEINT NOT NULL,
+    to_symbol VARCHAR(30) NOT NULL,
+    type VARCHAR(4) CHECK (type IN ('buy', 'sell')) NOT NULL,
+    spender ADDRESS NOT NULL,
+    recipient ADDRESS NOT NULL,
+    campaign_id TEXT REFERENCES ninelives_campaigns_1 (id),
+    campaign_content JSONB NOT NULL
+);
+
+CREATE FUNCTION
+NINELIVES_FUN_NINELIVES_EVENTS_SHARES_MINTED_TO_NINELIVES_BUYS_AND_SELLS_1()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$
+DECLARE
+    campaign_id TEXT;
+    campaign_content JSONB;
+BEGIN
+ 	SELECT id, content
+    INTO campaign_id, campaign_content
+    FROM ninelives_campaigns_1
+    WHERE content::text LIKE CONCAT('%', '0x', LEFT(NEW.identifier, 16), '%');
+	
+	INSERT INTO ninelives_buys_and_sells_1 (
+		transaction_hash,
+		created_by,
+		block_hash,
+		block_number,
+		emitter_addr,
+		from_amount,
+		from_symbol,
+		to_amount,
+		to_symbol,
+		type,
+		spender,
+		recipient,
+		campaign_id,
+		campaign_content
+	)
+	VALUES (
+		NEW.transaction_hash,
+		NEW.created_by,
+		NEW.block_hash,
+		NEW.block_number,
+		NEW.emitter_addr,
+		NEW.fusdc_spent,
+		'FUSDC',
+		NEW.share_amount,
+		'9#Share',
+		'buy',
+		NEW.spender,
+		NEW.recipient,
+		campaign_id,
+		campaign_content
+	);
+	RETURN NEW;
+END $$;
+
+CREATE TRIGGER
+ninelives_trigger_ninelives_events_shares_minted_to_ninelives_buys_and_sells_1
+AFTER INSERT ON ninelives_events_shares_minted
+FOR EACH ROW
+EXECUTE FUNCTION
+NINELIVES_FUN_NINELIVES_EVENTS_SHARES_MINTED_TO_NINELIVES_BUYS_AND_SELLS_1();
+-- migrate:down
