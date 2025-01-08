b3fc764ae5739a19c52bdf553892f190117ee87c
ogous <oguzxan.ali@gmail.com>
2025-01-08 17:51:44 +0300

    chore: add buy helper abi

---
+++ web/src/config/abi/buyHelper.ts
@@ -0,0 +1,68 @@
+const buyHelperAbi = [
+  {
+    type: "constructor",
+    inputs: [
+      {
+        name: "_factory",
+        type: "address",
+        internalType: "contract INineLivesFactory",
+      },
+      {
+        name: "_longtail",
+        type: "address",
+        internalType: "contract ILongtail",
+      },
+      {
+        name: "_fusdc",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "_weth",
+        type: "address",
+        internalType: "contract IWETH10",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "mint",
+    inputs: [
+      {
+        name: "_id",
+        type: "bytes32",
+        internalType: "bytes32",
+      },
+      {
+        name: "_asset",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "_outcome",
+        type: "bytes8",
+        internalType: "bytes8",
+      },
+      {
+        name: "_minShareOut",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "_amount",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "payable",
+  },
+] as const;
+export default buyHelperAbi;
