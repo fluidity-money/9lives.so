096df77c68b118afcf50c82b75a5d38b2cf934a6
bayge <alex@fluidity.money>
2025-01-20 14:13:20 +1030

    Anywhere in the box to open the campaign

--- web/src/components/campaign/campaignItem.tsx
+++ web/src/components/campaign/campaignItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
+
 import SelectedOutcome from "./selectedOutcome";
 import CampaignBody from "./campaignBody";
 import {
@@ -17,21 +19,23 @@ export default function CampaignItem({ data }: CampaignItemProps) {
   const [selectedOutcome, setSelectedOutcome] = useState<SelectedOutcomeType>();
 
   return (
-    <div className="flex flex-col gap-2 rounded-[3px] border-2 border-9black bg-9gray p-3 shadow-9card">
-      {selectedOutcome ? (
-        <SelectedOutcome
-          campaignId={data.identifier}
-          selectedState={selectedOutcome.state}
-          data={
-            data.outcomes.find(
-              (o) => o.identifier === selectedOutcome.id,
-            )! as Outcome
-          }
-          setSelectedOutcome={setSelectedOutcome}
-        />
-      ) : (
-        <CampaignBody data={data} setSelectedOutcome={setSelectedOutcome} />
-      )}
-    </div>
+    <Link href={`/campaign/${data.identifier}`} className="flex flex-1">
+      <div className="flex flex-col gap-2 rounded-[3px] border-2 border-9black bg-9gray p-3 shadow-9card">
+        {selectedOutcome ? (
+          <SelectedOutcome
+            campaignId={data.identifier}
+            selectedState={selectedOutcome.state}
+            data={
+              data.outcomes.find(
+                (o) => o.identifier === selectedOutcome.id,
+              )! as Outcome
+            }
+            setSelectedOutcome={setSelectedOutcome}
+          />
+        ) : (
+          <CampaignBody data={data} setSelectedOutcome={setSelectedOutcome} />
+        )}
+      </div>
+    </Link>
   );
 }
