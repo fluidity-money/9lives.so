a4b3a81af7f93dd5f7e09859d9ae186ae5d35d9a
ogous <oguzxan.ali@gmail.com>
2025-01-21 14:42:13 +0300

    pop up connect modal if user navigate a campaign  without an account

--- web/src/providers/contextInjector.tsx
+++ web/src/providers/contextInjector.tsx
@@ -5,9 +5,11 @@ import { useEffect } from "react";
 import { setTag, setUser } from "@sentry/nextjs";
 import { useDegenStore } from "@/stores/degenStore";
 import { usePathname } from "next/navigation";
+import useConnectWallet from "@/hooks/useConnectWallet";
 
 export default function ContextInjector() {
   const account = useActiveAccount();
+  const { connect } = useConnectWallet();
   const chain = useActiveWalletChain();
   const degenModeEnabled = useDegenStore((state) => state.degenModeEnabled);
   const pathname = usePathname();
@@ -33,12 +35,20 @@ export default function ContextInjector() {
     else body.classList.remove("degen-mode");
   }, [degenModeEnabled]);
 
+  // add margin for fixed action component on campaign details
   useEffect(() => {
     const footer = document.getElementsByTagName("footer")[0];
     if (pathname.startsWith("/campaign/"))
       footer.classList.add("mint-box-margin");
     else footer.classList.remove("mint-box-margin");
   }, [pathname]);
+
+  // pop up connect modal if user navigates any campaign page
+  useEffect(() => {
+    if (pathname.startsWith("/campaign/") && !account) {
+      connect();
+    }
+  }, [pathname, account]);
 
   return null;
 }
