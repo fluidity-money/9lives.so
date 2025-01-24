689f155449d71269d2947cbcdd078b39c1f665c8
bayge <alex@fluidity.money>
2025-01-24 19:23:08 +1030

    Rename update winners function to have ninelives prefix

--- db/migrations/1737541181-update-campaign-for-winner.sql
+++ db/migrations/1737541181-update-campaign-for-winner.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 
-CREATE FUNCTION update_campaign_for_winner()
+CREATE FUNCTION ninelives_update_campaign_for_winner()
 RETURNS TRIGGER LANGUAGE plpgsql AS $$
 BEGIN
 	UPDATE ninelives_buys_and_sells_1
@@ -13,6 +13,6 @@ END $$;
 CREATE TRIGGER ninelives_trigger_update_campaign_for_winner
 AFTER INSERT ON ninelives_events_outcome_decided
 FOR EACH ROW
-EXECUTE FUNCTION update_campaign_for_winner();
+EXECUTE FUNCTION ninelives_update_campaign_for_winner();
 
 -- migrate:down
