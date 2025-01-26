e5cd5fb216f1bf7c1638b3ed7f87310a1aa5b593
bayge <alex@fluidity.money>
2025-01-26 22:21:50 +1030

    Disable create campaign link

--- web/src/components/createCampaign/createCampaignHeaderButton.tsx
+++ web/src/components/createCampaign/createCampaignHeaderButton.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function CreateCampaingButton() {
   return (
     <div className="h-10">
-      <Link href={"#"} disabled>
+      <Link href={"#"}>
         <button
           className="hidden items-center justify-center px-4 font-chicago leading-10 underline md:flex"
           disabled
