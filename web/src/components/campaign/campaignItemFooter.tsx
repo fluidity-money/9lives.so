80cb33f420ac87d4bddd051e0464ee977e37032d
bayge <alex@fluidity.money>
2025-01-28 20:53:36 +1030

    Rename fUSDC to USDC

--- web/src/components/campaign/campaignItemFooter.tsx
+++ web/src/components/campaign/campaignItemFooter.tsx
@@ -8,7 +8,7 @@ export default function CampaignItemFooter({
 }: CampaignItemFooterProps) {
   return (
     <div className="flex items-center justify-between gap-2 font-geneva text-[10px] uppercase leading-3 tracking-wide text-[#808080]">
-      <span>{formatFusdc(volume + 2e6)} fUSDC Vol.</span>
+      <span>{formatFusdc(volume + 2e6)} USDC Vol.</span>
     </div>
   );
 }
