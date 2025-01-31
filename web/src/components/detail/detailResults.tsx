63720264167f399a808a397b7a2596077a7c87f2
ogous <oguzxan.ali@gmail.com>
2025-01-31 17:05:52 +0200

    add mobile optimization for claim component

--- web/src/components/detail/detailResults.tsx
+++ web/src/components/detail/detailResults.tsx
@@ -13,6 +13,8 @@ import YesOutcomeImg from "#/images/yes-outcome.svg";
 import NoOutcomeImg from "#/images/no-outcome.svg";
 import formatFusdc from "@/utils/formatFusdc";
 import useChances from "@/hooks/useChances";
+import DownIcon from "#/icons/down-caret.svg";
+import { combineClass } from "@/utils/combineClass";
 
 interface DetailResultsProps {
   data: CampaignDetail;
@@ -21,6 +23,7 @@ export default function DetailResults({ data }: DetailResultsProps) {
   const account = useActiveAccount();
   const { connect, isConnecting } = useConnectWallet();
   const [isClaiming, setIsClaiming] = useState(false);
+  const [minimized, setMinimized] = useState(true);
   const { data: positionData } = usePositions({
     tradingAddr: data.poolAddress,
     outcomes: data.outcomes,
@@ -83,69 +86,91 @@ export default function DetailResults({ data }: DetailResultsProps) {
   }
 
   return (
-    <ShadowCard className="sticky top-0 z-10 flex flex-col gap-4 p-4">
-      <div className="flex items-center gap-4">
-        <div className="relative">
-          <Image
-            alt=""
-            width={20}
-            className="absolute inset-x-0 -top-2 mx-auto h-auto"
-            src={CrownImg}
-          />
-          <div className="size-10 overflow-hidden rounded-full">
+    <ShadowCard className="fixed inset-x-0 bottom-0 z-10 flex flex-col gap-4 p-4 md:relative">
+      <div
+        className="absolute right-4 top-[-41px] rounded-t-sm border border-b-0 border-9black bg-9layer p-2 md:hidden"
+        onClick={() => setMinimized(!minimized)}
+      >
+        <Image
+          src={DownIcon}
+          alt=""
+          className={combineClass(minimized && "rotate-180")}
+        />
+      </div>
+      <div
+        className={combineClass(
+          minimized ? "flex-row" : "flex-col",
+          "flex gap-4",
+        )}
+      >
+        <div className="flex items-center gap-4">
+          <div className="relative">
             <Image
-              width={40}
-              height={40}
-              alt={winner.name}
-              src={
-                data.isYesNo
-                  ? winner.name === "Yes"
-                    ? YesOutcomeImg
-                    : NoOutcomeImg
-                  : winner.picture
-              }
-              className="size-full object-cover"
+              alt=""
+              width={20}
+              className="absolute inset-x-0 -top-2 mx-auto h-auto"
+              src={CrownImg}
             />
+            <div className="size-10 overflow-hidden rounded-full">
+              <Image
+                width={40}
+                height={40}
+                alt={winner.name}
+                src={
+                  data.isYesNo
+                    ? winner.name === "Yes"
+                      ? YesOutcomeImg
+                      : NoOutcomeImg
+                    : winner.picture
+                }
+                className="size-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-1">
-          <h3 className="font-chicago text-base font-normal text-9black">
-            {winner.name}
-          </h3>
-          <div className="flex items-center gap-1 font-geneva text-[10px]">
-            <span className="uppercase">Chance</span>
-            <span className="bg-9green px-1 py-0.5">
-              {winnerChance ? Math.round(winnerChance) : "?"}%
+          <div className="flex flex-col gap-1">
+            <h3 className="font-chicago text-base font-normal text-9black">
+              {winner.name}
+            </h3>
+            <div className="flex items-center gap-1 font-geneva text-[10px]">
+              <span className="uppercase">Chance</span>
+              <span className="bg-9green px-1 py-0.5">
+                {winnerChance ? Math.round(winnerChance) : "?"}%
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="font-chicago uppercase">Claimable Rewards</span>
+          <div className="flex items-center gap-1">
+            <span className="font-chicago text-2xl">
+              $
+              {account
+                ? accountShares
+                  ? (accountShares * avgPrice).toFixed(3)
+                  : 0
+                : "?"}
             </span>
+            <Image src={SparkleImg} alt="" width={23} className="h-auto" />
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-between">
-        <span className="font-chicago uppercase">Claimable Rewards</span>
-        <div className="flex items-center gap-1">
-          <span className="font-chicago text-2xl">
-            $
-            {account
-              ? accountShares
-                ? (accountShares * avgPrice).toFixed(3)
-                : 0
-              : "?"}
-          </span>
-          <Image src={SparkleImg} alt="" width={23} className="h-auto" />
+      {minimized ? null : (
+        <div className="flex flex-col gap-4 bg-9gray p-5 text-xs shadow-9orderSummary">
+          <span className="font-chicago uppercase">Reward Breakdown</span>
+          <ul className="flex flex-col gap-1 text-gray-500">
+            {rewardBreakdown.map((item) => (
+              <li
+                className="flex items-center justify-between"
+                key={item.title}
+              >
+                <strong>{item.title}</strong>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-      <div className="flex flex-col gap-4 bg-9gray p-5 text-xs shadow-9orderSummary">
-        <span className="font-chicago uppercase">Reward Breakdown</span>
-        <ul className="flex flex-col gap-1 text-gray-500">
-          {rewardBreakdown.map((item) => (
-            <li className="flex items-center justify-between" key={item.title}>
-              <strong>{item.title}</strong>
-              <span>{item.value}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      )}
       <Button
         disabled={noClaim || isConnecting || isClaiming}
         title={
