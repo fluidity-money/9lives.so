87d9e4547923326ec6128b381c93c7c2d271e549
ogous <oguzxan.ali@gmail.com>
2025-02-05 15:50:08 +0300

    add helper files for local graph server

--- db/docker.sh
+++ db/docker.sh
@@ -8,5 +8,6 @@ docker run \
 	-e POSTGRES_USER=${POSTGRES_USER:-superposition} \
 	-e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-superposition} \
 	-p 5432:5432 \
+	# -v $(pwd)/volume:/var/lib/postgresql/data \
 	-t ninelives/database \
 	-c log_statement=all
