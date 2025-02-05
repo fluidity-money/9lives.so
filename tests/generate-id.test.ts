79d3600711f64bd3a45cccd3d8f92508dd2264f1
ogous <oguzxan.ali@gmail.com>
2025-02-05 17:41:16 +0300

    update generate id test

--- tests/generate-id.test.ts
+++ tests/generate-id.test.ts
@@ -1,17 +1,16 @@
 import assert from 'assert';
-import { generateId } from '../web/src/utils/generateId';
+import { generateOutcomeId } from '../web/src/utils/generateId';
 import { exec } from "child_process";
 import { describe, it } from "node:test"
 import { promisify } from 'util';
 
 describe('Node,js and Go id generation should be same', async () => {
     const name = 'Yes'
-    const description = ''
     const seed = 2429095232877049
-    const command = `go run scripts/generate-id.go ${name} "${description}" ${seed}`;
+    const command = `go run scripts/generate-id.go ${name} ${seed}`;
     const getGoId = promisify(exec)
     const { stdout } = await getGoId(command)
     const stdoutArr = stdout.trim().split(',')
     const generatedGoId = stdoutArr[stdoutArr.length - 1]
-    assert.equal(generatedGoId, generateId(name, description, seed), 'ids have to be equal')
+    assert.equal(generatedGoId, generateOutcomeId(name, seed), 'ids have to be equal')
 })
