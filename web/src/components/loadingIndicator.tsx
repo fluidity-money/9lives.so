41a01b11f4977f2657fc9419dea82653d0a727e5
ogous <oguzxan.ali@gmail.com>
2025-02-06 17:07:26 +0300

    componentize loading indicator

---
+++ web/src/components/loadingIndicator.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import LoadingImage from "#/icons/loading.svg";
+export default function LoadingIndicator() {
+  return (
+    <div className="h-100 flex items-center justify-center">
+      <Image
+        src={LoadingImage}
+        className="animate-spin"
+        alt="Loading"
+        width={16}
+      />
+      <span className="ml-2 font-geneva text-sm">Loading...</span>
+    </div>
+  );
+}
