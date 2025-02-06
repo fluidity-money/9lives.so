166af93572ea939f5aa5c41cf060b2b07f39f7f6
ogous <oguzxan.ali@gmail.com>
2025-02-06 17:07:58 +0300

    add search component to ui

--- web/src/components/campaign/campaignList.tsx
+++ web/src/components/campaign/campaignList.tsx
@@ -4,13 +4,18 @@ import CampaignItem from "./campaignItem";
 import { useQuery } from "@tanstack/react-query";
 import { combineClass } from "@/utils/combineClass";
 import { useDegenStore } from "@/stores/degenStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Select } from "@headlessui/react";
 import { requestCampaignList } from "@/providers/graphqlClient";
 import Image from "next/image";
-import LoadingImage from "#/icons/loading.svg";
+import Input from "../themed/input";
+import CloseIcon from "#/icons/close.svg";
+import SearchIcon from "#/icons/search.svg";
+import LoadingIndicator from "../loadingIndicator";
 export default function CampaignList() {
   const [orderBy, setOrderBy] = useState<CampaignFilters["orderBy"]>("volume");
+  const [searchTermInput, setSearchTermInput] = useState("");
+  const [searcTermFilter, setSearcTermFilter] = useState("");
   const { data, isLoading } = useQuery<Campaign[]>({
     queryKey: ["campaigns", orderBy],
     queryFn: async () => {
@@ -33,33 +38,59 @@ export default function CampaignList() {
       ["newest", "Newest"],
       ["volume", "Volume"],
     ];
+  useEffect(() => {
+    if (searchTermInput) {
+      const timer = setTimeout(() => setSearcTermFilter(searchTermInput), 300);
+      return () => clearTimeout(timer);
+    }
+  }, [searchTermInput]);
   return (
     <>
-      <Select
-        name="order-by"
-        aria-label="order-by"
-        onChange={(e) =>
-          setOrderBy(e.target.value as CampaignFilters["orderBy"])
-        }
-        value={orderBy}
-        className="mb-4 rounded-sm border border-9black px-2 py-1 font-chicago text-xs shadow-9btnSecondaryIdle focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-      >
-        {orderByFilters.map(([key, title]) => (
-          <option key={key} value={key}>
-            Sort: {title}
-          </option>
-        ))}
-      </Select>
-      {isLoading ? (
-        <div className="h-100 flex items-center justify-center">
+      <div className="mb-4 flex flex-1 items-center gap-4">
+        <Select
+          name="order-by"
+          aria-label="order-by"
+          onChange={(e) =>
+            setOrderBy(e.target.value as CampaignFilters["orderBy"])
+          }
+          value={orderBy}
+          className="flex items-center self-stretch rounded-sm border border-9black px-2 py-1 font-chicago text-xs shadow-9btnSecondaryIdle focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+        >
+          {orderByFilters.map(([key, title]) => (
+            <option key={key} value={key}>
+              Sort: {title}
+            </option>
+          ))}
+        </Select>
+        <div className="relative flex flex-1 items-center">
           <Image
-            src={LoadingImage}
-            className="animate-spin"
-            alt="Loading"
-            width={16}
+            src={SearchIcon}
+            alt=""
+            width={18}
+            className="absolute left-2"
           />
-          <span className="ml-2 font-geneva text-sm">Loading...</span>
+          <Input
+            value={searchTermInput}
+            placeholder="Search campaigns"
+            onChange={(e) => setSearchTermInput(e.target.value)}
+            className={"flex-1 pl-8"}
+          />
+          {searchTermInput ? (
+            <Image
+              src={CloseIcon}
+              alt=""
+              width={20}
+              className="absolute right-1 cursor-pointer"
+              onClick={() => {
+                setSearcTermFilter("");
+                setSearchTermInput("");
+              }}
+            />
+          ) : null}
         </div>
+      </div>
+      {isLoading ? (
+        <LoadingIndicator />
       ) : (
         <div
           className={combineClass(
