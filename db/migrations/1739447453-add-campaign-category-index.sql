51f563c93da042a31376d438e791837cf86f28c6
ogous <oguzxan.ali@gmail.com>
2025-02-13 15:10:28 +0300

    add gin index for faster category filters

--- db/migrations/1739447453-add-campaign-category-index.sql
+++ db/migrations/1739447453-add-campaign-category-index.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 
 CREATE INDEX ninelives_campaign_categories_idx
-ON ninelives_campaigns_1 USING gin ((content ->> 'categories') jsonb_ops);
+ON ninelives_campaigns_1 USING gin ((content -> 'categories'));
 
 -- migrate:down
