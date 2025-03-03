e69c5955aae0a45d88d3538a6efe157887dc4968
ogous <oguzxan.ali@gmail.com>
2025-03-03 14:39:07 +0300

    add unit test of generate commit

--- tests/generate-commit.test.ts
+++ tests/generate-commit.test.ts
@@ -1,19 +1,19 @@
 import assert from 'assert';
 import { generateCommit } from '../web/src/utils/generateCommit';
-import { exec } from "child_process";
 import { describe, it } from "node:test"
-import { promisify } from 'util';
 
 describe('Node.js and Rust commit hash generation should be the same', async () => {
-    const committer = '0xa56333f8738fcb8247e8a27aed8a207d981f79c0'; 
-    const outcome = '0x32ff350d90e67a2e';
-    const seed = 2429095232877049n; 
+    const committer = '0xd8e78951d5bd1dee358d9772464d45025338b6bf';
+    const outcome = '0x21e44c00f545271b';
+    const seed1 = 123;
+    const expectedHash1 = "0x05e244a43f9f06def22e334260ba61a0c9a147fe05ea4aeda79cd7f6212e53de"
+    it("Hash should be the same", () => assert.equal(generateCommit(committer, outcome, seed1), expectedHash1, 'Commit hashes must be equal'))
 
-    const command = `rust-script scripts/generate_commit.rs ${committer} ${outcome} ${seed}`;
-    const getRustCommit = promisify(exec);
-    const { stdout } = await getRustCommit(command);
-    
-    const generatedRustCommit = stdout.trim();
+    const seed2 = BigInt(9007199254740991);
+    const expectedHash2 = "0xe64424daa61a96b362f80495e452f87b684e374069c53af3d08c9758db8c2a98"
+    it("Hash should be the same", () => assert.equal(generateCommit(committer, outcome, seed2), expectedHash2, 'Commit hashes must be equal'))
 
-    assert.equal(generatedRustCommit, generateCommit(committer, outcome, seed), 'Commit hashes must be equal');
+    const seed3 = BigInt("340282366920938463463374607431768211454") // or 340282366920938463463374607431768211454n
+    const expectedHash3 = "0xcd5cc073f288e1a22a42327b6831eb29b0c09bbf7b8a95386d0b55e2a24be97f"
+    it("Hash should be the same", () => assert.equal(generateCommit(committer, outcome, seed3), expectedHash3, 'Commit hashes must be equal'))
 });
--- web/src/utils/generateCommit.ts
+++ web/src/utils/generateCommit.ts
@@ -4,19 +4,17 @@ import { Buffer } from "buffer";
 export function generateCommit(
   committer: string,
   outcome: string,
-  seed: bigint,
-): string {
+  seed: bigint | number,
+): `0x${string}` {
   if (!/^0x[a-fA-F0-9]{40}$/.test(committer)) {
     throw new Error("Invalid Ethereum address length");
   }
   if (!/^0x[a-fA-F0-9]{16}$/.test(outcome)) {
     throw new Error("Outcome must be a fixed 8-byte hex string (0x prefixed)");
   }
-
   const committerBuffer = Buffer.from(committer.slice(2), "hex"); // 20 byte
   const outcomeBuffer = Buffer.from(outcome.slice(2), "hex"); // 8 byte
-  const seedBuffer = Buffer.alloc(32);
-  seedBuffer.writeBigUInt64BE(BigInt(seed));
+  const seedBuffer = Buffer.from(seed.toString(16).padStart(64, "0"), "hex"); // 32 byte
   const concatenated = Buffer.concat([
     committerBuffer,
     outcomeBuffer,
@@ -26,5 +24,5 @@ export function generateCommit(
     .update(concatenated)
     .digest()
     .toString("hex");
-  return hash;
+  return `0x${hash}`;
 }
