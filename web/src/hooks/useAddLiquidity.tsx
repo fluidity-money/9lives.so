45cfc264cd75db787d179adba37bb0aa9112a4a3
ogous <oguzxan.ali@gmail.com>
2025-05-02 13:25:28 +0300

    add liquidity hook

---
+++ web/src/hooks/useAddLiquidity.tsx
@@ -0,0 +1,93 @@
+import config from "@/config";
+import tradingAbi from "@/config/abi/trading";
+import { Outcome } from "@/types";
+import { EVENTS, track } from "@/utils/analytics";
+import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import {
+  getContract,
+  prepareContractCall,
+  sendTransaction,
+  simulateTransaction,
+  toUnits,
+  ZERO_ADDRESS,
+} from "thirdweb";
+import { Account } from "thirdweb/wallets";
+
+export default function useAddLiquidity({
+  tradingAddr,
+  campaignId,
+}: {
+  tradingAddr: `0x${string}`;
+  campaignId: `0x${string}`;
+}) {
+  const queryClient = useQueryClient();
+  const addLiquidity = async (account: Account, fusdc: string) =>
+    toast.promise(
+      new Promise(async (res, rej) => {
+        try {
+          const amount = toUnits(fusdc, config.contracts.decimals.shares);
+          const allowanceTx = prepareContractCall({
+            contract: config.contracts.fusdc,
+            method: "allowance",
+            params: [account.address, tradingAddr],
+          });
+          const allowance = await simulateTransaction({
+            transaction: allowanceTx,
+            account: account,
+          });
+          if (amount > allowance) {
+            const approveTx = prepareContractCall({
+              contract: config.contracts.fusdc,
+              method: "approve",
+              params: [tradingAddr, amount],
+            });
+            await sendTransaction({
+              transaction: approveTx,
+              account,
+            });
+          }
+          const tradingContract = getContract({
+            abi: tradingAbi,
+            address: tradingAddr,
+            client: config.thirdweb.client,
+            chain: config.chains.currentChain,
+          });
+          const addLiquidityTx = prepareContractCall({
+            contract: tradingContract,
+            method: "addLiquidityPermit",
+            params: [
+              amount,
+              account.address,
+              BigInt(0),
+              0,
+              ZERO_ADDRESS,
+              ZERO_ADDRESS,
+            ],
+          });
+          await sendTransaction({
+            transaction: addLiquidityTx,
+            account,
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["campaign", campaignId],
+          });
+          track(EVENTS.ADD_LIQUIDITY, {
+            wallet: account.address,
+            amount,
+            tradingAddr,
+            campaignId,
+          });
+          res(null);
+        } catch (e) {
+          rej(e);
+        }
+      }),
+      {
+        loading: "Adding liquidity...",
+        success: "Liquidity added successfully!",
+        error: "Failed to add.",
+      },
+    );
+  return { addLiquidity };
+}
