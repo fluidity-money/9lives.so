405624f7db6855e3d953c3fa7bb18f244408cde6
ogous <oguzxan.ali@gmail.com>
2025-05-06 10:28:52 +0300

    add sell dialog

---
+++ web/src/components/sellDialog.tsx
@@ -0,0 +1,104 @@
+import { useForm } from "react-hook-form";
+import Button from "./themed/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { FormEvent, useState } from "react";
+import { useActiveAccount } from "thirdweb/react";
+import useConnectWallet from "@/hooks/useConnectWallet";
+import { Account } from "thirdweb/wallets";
+import useAddLiquidity from "@/hooks/useAddLiquidity";
+import { Field } from "@headlessui/react";
+import Label from "./themed/label";
+import AssetSelector from "./assetSelector";
+import Input from "./themed/input";
+import { combineClass } from "@/utils/combineClass";
+import ErrorInfo from "./themed/errorInfo";
+
+export default function SellDialog({
+  name,
+  close,
+  campaignId,
+  tradingAddr,
+}: {
+  name: string;
+  close: () => void;
+  campaignId: `0x${string}`;
+  tradingAddr: `0x${string}`;
+}) {
+  const account = useActiveAccount();
+  const { connect } = useConnectWallet();
+  const [isFunding, setIsFunding] = useState(false);
+  const formSchema = z.object({
+    seedLiquidity: z.preprocess((val) => Number(val), z.number().min(1)),
+  });
+  type FormData = z.infer<typeof formSchema>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      seedLiquidity: 1,
+    },
+  });
+  const { addLiquidity } = useAddLiquidity({
+    campaignId,
+    tradingAddr,
+  });
+  const onSubmit = async (input: FormData, account: Account) => {
+    try {
+      setIsFunding(true);
+      await addLiquidity(account!, input.seedLiquidity.toString());
+      close();
+    } finally {
+      setIsFunding(false);
+    }
+  };
+  const handleSubmitWithAccount = (e: FormEvent) => {
+    if (!account) {
+      e.preventDefault();
+      connect();
+      return;
+    }
+    handleSubmit((data) => onSubmit(data, account))(e);
+  };
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-center font-chicago text-base">
+        Supply Liquidity to The Campaign
+      </p>
+      <p className="text-center font-chicago text-xl">{name}</p>
+      <p className="text-center text-xs">
+        Higher liquidty means better trading stability and lower slippage. You
+        can add liquidity to your campaign and earn provider rewards at any
+        time.
+      </p>
+      <Field className={"flex flex-col gap-2.5"}>
+        <Label text="Seed Liquidity" required />
+        <div className="flex gap-2.5">
+          <AssetSelector />
+          <Input
+            {...register("seedLiquidity")}
+            type="number"
+            min={1}
+            className={combineClass(
+              errors.seedLiquidity && "border-2 border-red-500",
+              "flex-1",
+            )}
+          />
+        </div>
+        {errors.seedLiquidity && (
+          <ErrorInfo text={errors.seedLiquidity.message} />
+        )}
+      </Field>
+      <Button
+        intent={"yes"}
+        title={isFunding ? "Loading..." : "Add Liquidity"}
+        size={"xlarge"}
+        disabled={isFunding}
+        onClick={handleSubmitWithAccount}
+      />
+    </div>
+  );
+}
