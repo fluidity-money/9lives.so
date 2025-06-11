a5ac3a2cb8382c297bffd0e3c177a4c56864c60d
ogous <oguzxan.ali@gmail.com>
2025-06-11 15:41:55 +0300

    update style of error indicator

--- web/src/components/errorIndicator.tsx
+++ web/src/components/errorIndicator.tsx
@@ -3,12 +3,7 @@ import ErrorImage from "#/icons/sad-face.svg";
 export default function ErrorIndicator({ msg }: { msg?: string }) {
   return (
     <div className="flex items-center justify-center">
-      <Image
-        src={ErrorImage}
-        className="animate-spin"
-        alt="Loading"
-        width={16}
-      />
+      <Image src={ErrorImage} alt="" width={20} />
       <span className="ml-2 font-geneva text-sm">
         {msg ?? "Ups something went wrong."}
       </span>
