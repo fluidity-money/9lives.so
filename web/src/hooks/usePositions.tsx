c13b339e7c149193d2774e16c14ddb2f72260422
ogous <oguzxan.ali@gmail.com>
2025-06-11 18:49:08 +0300

    refactor usePosition to use new method

--- web/src/hooks/usePositions.tsx
+++ web/src/hooks/usePositions.tsx
@@ -1,62 +1,34 @@
 import config from "@/config";
-import ERC20Abi from "@/config/abi/erc20";
 import { Outcome } from "@/types";
 import formatFusdc from "@/utils/formatFusdc";
 import { useQuery } from "@tanstack/react-query";
-// import { zeroPadBytes, zeroPadValue } from "ethers";
-import {
-  getContract,
-  prepareContractCall,
-  simulateTransaction,
-  // ZERO_ADDRESS,
-} from "thirdweb";
+import { prepareContractCall, simulateTransaction } from "thirdweb";
 import { Account } from "thirdweb/wallets";
+
 async function fetchPositions(
   tradingAddr: `0x${string}`,
   outcomes: Outcome[],
   account?: Account,
 ) {
   if (!account) return [];
-  // const words = outcomes.map((i) => zeroPadValue(i.identifier, 32));
-  // const balances = (await simulateTransaction({
-  //   transaction: prepareContractCall({
-  //     contract: config.contracts.lens,
-  //     method: "balancesWithFactoryAndHash",
-  //     params: [
-  //       ZERO_ADDRESS,
-  //       zeroPadBytes("0x00", 32) as `0x${string}`,
-  //       tradingAddr,
-  //       words as `0x${string}`[],
-  //       account.address,
-  //     ],
-  //   }),
-  // })) as bigint[];
-  const balances = await Promise.all<string>(
-    outcomes.map((o) =>
-      simulateTransaction({
-        transaction: prepareContractCall({
-          contract: getContract({
-            address: o.share.address,
-            abi: ERC20Abi,
-            client: config.thirdweb.client,
-            chain: config.chains.currentChain,
-          }),
-          method: "balanceOf",
-          params: [account.address],
-        }),
+  const balances = (
+    (await simulateTransaction({
+      transaction: prepareContractCall({
+        contract: config.contracts.lens,
+        method: "balancesForAll",
+        params: [[tradingAddr]],
       }),
-    ),
-  );
+    })) as { amount: string; id: `0x${string}`; name: string }[]
+  ).filter((i) => BigInt(i.amount) > BigInt(0));
 
-  const mintedPositions = outcomes
-    .map((outcome, idx) => ({
-      id: outcome.identifier,
-      shareAddress: outcome.share.address,
-      name: outcome.name,
-      balance: formatFusdc(Number(balances[idx]), 2),
-      balanceRaw: BigInt(balances[idx]),
-    }))
-    .filter((item) => item.balance !== "0.00");
+  const mintedPositions = balances.map((b) => ({
+    id: b.id,
+    shareAddress:
+      outcomes.find((o) => o.identifier === b.id)?.share.address ?? "0x",
+    name: b.name,
+    balance: formatFusdc(Number(b.amount), 2),
+    balanceRaw: BigInt(b.amount),
+  }));
 
   return mintedPositions;
 }
