04f049cc692bf032aa8c14df308cd70a0090b710
bayge <alex@fluidity.money>
2025-06-20 17:24:57 +0700

    Rollback featured search

--- cmd/graphql.ethereum/graph/schema.resolvers.go
+++ cmd/graphql.ethereum/graph/schema.resolvers.go
@@ -1365,7 +1365,7 @@ func (r *queryResolver) FeaturedCampaign(ctx context.Context, limit *int) ([]typ
 	}
 	err := r.DB.Raw(`
 	SELECT *
-	FROM private_ninelives_campaigns_sorted_by_breakout_3
+	FROM private_ninelives_campaigns_sorted_by_breakout_2
 	WHERE content->>'winner' IS NULL
 	LIMIT ?
 	`, n).Scan(&campaigns).Error
