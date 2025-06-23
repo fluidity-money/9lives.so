4d65117099b85242a9fd744b13efb6814a7c41b3
ogous <oguzxan.ali@gmail.com>
2025-06-23 17:43:16 +0300

    add buy with paymaster hook

---
+++ web/src/hooks/useBuyWithPaymaster.tsx
@@ -0,0 +1,92 @@
+import config from "@/config";
+import { prepareContractCall, simulateTransaction } from "thirdweb";
+import { toUnits } from "thirdweb/utils";
+import { Account } from "thirdweb/wallets";
+import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { Outcome } from "@/types";
+import { track, EVENTS } from "@/utils/analytics";
+import useRequestPaymaster from "./useRequestPaymaster";
+
+const useBuyWithPaymaster = ({
+  shareAddr,
+  tradingAddr,
+  campaignId,
+  outcomeId,
+  openFundModal,
+}: {
+  shareAddr: `0x${string}`;
+  tradingAddr: `0x${string}`;
+  outcomeId: `0x${string}`;
+  campaignId: `0x${string}`;
+  openFundModal: () => void;
+}) => {
+  const queryClient = useQueryClient();
+  const { requestPaymaster } = useRequestPaymaster();
+  const buy = async (account: Account, fusdc: number, outcomes: Outcome[]) =>
+    toast.promise(
+      new Promise(async (res, rej) => {
+        try {
+          const amount = toUnits(
+            fusdc.toString(),
+            config.contracts.decimals.fusdc,
+          ).toString();
+          const userBalanceTx = prepareContractCall({
+            contract: config.contracts.fusdc,
+            method: "balanceOf",
+            params: [account?.address],
+          });
+          const userBalance = await simulateTransaction({
+            transaction: userBalanceTx,
+            account,
+          });
+          if (amount > userBalance) {
+            openFundModal();
+            throw new Error("You dont have enough USDC.");
+          }
+          const ticketId = await requestPaymaster({
+            amountToSpend: amount,
+            outcome: outcomeId,
+            opType: "MINT",
+            tradingAddr: tradingAddr,
+          });
+          const outcomeIds = outcomes.map((o) => o.identifier);
+          queryClient.invalidateQueries({
+            queryKey: ["positions", tradingAddr, outcomes, account],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["sharePrices", tradingAddr, outcomeIds],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["returnValue", shareAddr, tradingAddr, outcomeId, fusdc],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["campaign", campaignId],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["positionHistory", outcomeIds],
+          });
+          track(EVENTS.MINT, {
+            wallet: account.address,
+            amount: fusdc,
+            outcomeId,
+            shareAddr,
+            tradingAddr,
+          });
+          res(ticketId);
+        } catch (e) {
+          rej(e);
+        }
+      }),
+      {
+        loading: "Buying shares...",
+        success: "Shares bought successfully!",
+        error: (e: unknown) =>
+          `Create failed. ${e instanceof Error ? e.message : e instanceof String ? e : `Unknown error: ${JSON.stringify(e)}`}`,
+      },
+    );
+
+  return { buy };
+};
+
+export default useBuyWithPaymaster;
