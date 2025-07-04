b93ba68f0e7b41f219f0ef20d11cbe9a8b416641
bayge <alex@fluidity.money>
2025-07-04 18:58:12 +0930

    Fix the templating of the CTE

--- cmd/paymaster.ethereum/tmpl.go
+++ cmd/paymaster.ethereum/tmpl.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"strings"
 	"text/template"
-	"database/sql"
 )
 
 var logIdsTmpl = template.Must(
 	template.New("track id results").
 		Funcs(template.FuncMap{
 			"add": func(x, y int) int { return x + y },
+			"formatHash": func(hash sql.NullString) string {
+				if hash.Valid {
+					return "'" + hash.String + "'"
+				}
+				return "NULL"
+			},
 		}).
 		Parse(`
 WITH function_calls AS (
 	SELECT
 		poll_id,
 		success,
-		ninelives_paymaster_track_result_2(poll_id, success, transaction_hash)
+		hash,
+		ninelives_paymaster_track_result_2(poll_id, success, hash) as result
 	FROM (
-		VALUES {{range $i, $v := .}}({{ .Id}}, {{ .Success }}, '{{ .Hash.String }}'){{if not (eq (add $i 1) (len $))}},{{end}}{{end}}
-	) AS params(poll_id, success)
+		VALUES {{range $i, $v := .}}({{ .Id}}, {{ .Success }}, {{formatHash .Hash}}){{if not (eq (add $i 1) (len $))}},{{end}}{{end}}
+	) AS params(poll_id, success, hash)
 )
 SELECT poll_id, success FROM function_calls;
 `))
