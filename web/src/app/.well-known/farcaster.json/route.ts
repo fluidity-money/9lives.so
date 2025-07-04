6bad94db13a9bc8c196fceb02cf2ec97ee80a99f
bayge <alex@fluidity.money>
2025-07-05 00:21:42 +0930

    Prettier write format

--- web/src/app/.well-known/farcaster.json/route.ts
+++ web/src/app/.well-known/farcaster.json/route.ts
@@ -3,9 +3,11 @@ import appConfig from "../../../config";
 export async function GET() {
   const config = {
     accountAssociation: {
-      header: "eyJmaWQiOjE5Nzc4LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4NjVENUIxOWY5YzUzMDJlY0Y1YTVjM0Y1OTc1MUY0Mjk0M0EwMDc2YSJ9",
+      header:
+        "eyJmaWQiOjE5Nzc4LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4NjVENUIxOWY5YzUzMDJlY0Y1YTVjM0Y1OTc1MUY0Mjk0M0EwMDc2YSJ9",
       payload: "eyJkb21haW4iOiI5bGl2ZXMuc28ifQ",
-      signature: "By3HTQIP1T8XsTXTWmS59JUbYHLVcl+5Xf35D8kc291MMjvGkSOvejueGB4u4ViWDYFpXRqSS/Uy/xBYHlupdBw=",
+      signature:
+        "By3HTQIP1T8XsTXTWmS59JUbYHLVcl+5Xf35D8kc291MMjvGkSOvejueGB4u4ViWDYFpXRqSS/Uy/xBYHlupdBw=",
     },
     frame: appConfig.frame,
   };
