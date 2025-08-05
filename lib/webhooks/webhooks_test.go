5086fa6a45594894293e8f63583505fb4dea62bb
bayge <alex@fluidity.money>
2025-08-05 21:50:46 +0930

    Test byte encoding

--- lib/webhooks/webhooks_test.go
+++ lib/webhooks/webhooks_test.go
@@ -2,14 +2,13 @@ package webhooks
 
 import (
 	"testing"
-	"encoding/hex"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestWebhooksTemplate(t *testing.T) {
 	r, err := buildTwistJson("Hello world", F{
-		"hello", hex.EncodeToString([]byte("Hello!")),
+		"hello", []byte("Hello!"),
 	})
 	t.Logf("twist message: %v", r)
 	assert.NoError(t, err)
