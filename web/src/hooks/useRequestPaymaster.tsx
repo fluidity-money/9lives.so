ef59b27be546829e448d01559f12c2e5305c8a7a
ogous <oguzxan.ali@gmail.com>
2025-08-27 14:57:09 +0300

    improve check for allow/approve

--- web/src/hooks/useRequestPaymaster.tsx
+++ web/src/hooks/useRequestPaymaster.tsx
@@ -33,7 +33,7 @@ export default function useRequestPaymaster() {
     let permitV = 0;
     const deadline = Math.floor(Date.now() / 1000) + 3600;
     if (
-      params.amountToSpend &&
+      Number(params.amountToSpend) > 0 &&
       (params.opType === "MINT" || params.opType === "ADD_LIQUIDITY")
     ) {
       const allowanceTx = prepareContractCall({
