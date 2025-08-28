a1c4fb693daed9dee47599eecc42118067f07b0d
ogous <oguzxan.ali@gmail.com>
2025-08-28 18:52:01 +0300

    use parents font family and size

--- web/src/components/countdownTimer.tsx
+++ web/src/components/countdownTimer.tsx
@@ -3,8 +3,6 @@ import useCountdown from "@/hooks/useCountdown";
 export function CountdownTimer({ endTime }: { endTime: number }) {
   const timeleft = useCountdown(endTime);
   return (
-    <span className="bg-9red px-0.5 py-[1px] font-geneva text-xs text-9black">
-      {timeleft}
-    </span>
+    <span className="bg-9red px-0.5 py-[1px] text-9black">{timeleft}</span>
   );
 }
