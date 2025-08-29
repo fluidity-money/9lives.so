8e33857aa5f155538ad039665f25d13d683d1b2e
ogous <oguzxan.ali@gmail.com>
2025-08-29 14:53:17 +0300

    only return positive lped amount

--- cmd/graphql.ethereum/graph/schema.resolvers.go
+++ cmd/graphql.ethereum/graph/schema.resolvers.go
@@ -1562,6 +1562,7 @@ func (r *queryResolver) UserLPs(ctx context.Context, address string) ([]types.LP
 	LEFT JOIN
     ninelives_campaigns_1 AS nc
     ON nc.content->>'poolAddress' = added.emitter_addr
+	WHERE COALESCE(added.total_added, 0) - COALESCE(removed.total_removed, 0) > 0
 	`, address, address).Scan(&lps).Error
 	if err != nil {
 		slog.Error("Error getting user lps",
