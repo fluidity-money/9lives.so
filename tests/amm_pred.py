aa9910a783fa641000ae9c621921643d89adef33
bayge <alex@fluidity.money>
2025-09-11 14:14:20 +0900

    Add back fee taking to the reference implementation for burns

--- tests/amm_pred.py
+++ tests/amm_pred.py
@@ -322,11 +322,15 @@ class PredMarketNew:
 
         previous_shares = self.shares.copy()
 
+        fee = (amount * self.fees) / (1 - self.fees)
+        # Add fee to the amount
+        amount_with_fee = amount + fee
+
         # Update all shares with the purchase amount
         for i in range(len(self.shares)):
             if (i != outcome):
-                self.shares[i] -= amount
-                self.total_shares[i] -= amount
+                self.shares[i] -= amount_with_fee
+                self.total_shares[i] -= amount_with_fee
                 if (self.shares[i] < 0 or self.total_shares[i] < 0):
                     raise ArithmeticError("Underflow error: result is negative")
 
