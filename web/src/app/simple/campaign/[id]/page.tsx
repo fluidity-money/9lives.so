e172601306b011fa36ad4abcf825e6226c14317d
ogous <oguzxan.ali@gmail.com>
2025-10-15 18:52:25 +0300

    improve simple styling

--- web/src/app/simple/campaign/[id]/page.tsx
+++ web/src/app/simple/campaign/[id]/page.tsx
@@ -1,7 +1,9 @@
 import AssetPriceChart from "@/components/assetPriceChart";
 import config from "@/config";
 import { requestSimpleMarket } from "@/providers/graphqlClient";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import BTC from "#/images/tokens/btc.webp";
 type Params = Promise<{ id: string }>;
 export async function generateStaticParams() {
   return config.simpleMarkets.map((id) => ({
@@ -29,7 +31,21 @@ export default async function SimpleDetailPage({ params }: { params: Params }) {
   if (!data) notFound();
   return (
     <div>
-      <h1 className="font-chicago text-xl md:text-2xl">{data.name}</h1>
+      <div className="flex items-center gap-2">
+        <Image src={BTC} width={60} height={60} alt={data.name} />
+        <div className="flex flex-col gap-1">
+          <h1 className="font-chicago text-xl md:text-2xl">{data.name}</h1>
+          <span className="font-geneva text-xs uppercase text-[#808080]">
+            {new Date(data.starting).toLocaleString("default", {
+              hour: "numeric",
+            })}{" "}
+            -{" "}
+            {new Date(data.ending).toLocaleString("default", {
+              hour: "numeric",
+            })}
+          </span>
+        </div>
+      </div>
       <AssetPriceChart
         basePrice={data.basePrice}
         id={data.identifier}
