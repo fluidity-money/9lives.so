c081f433772cba8b49f317526a74043b78860eac
ogous <oguzxan.ali@gmail.com>
2025-11-05 17:17:37 +0300

    add generic og image to dppm markets

--- web/src/app/simple/campaign/[id]/farcaster-image/route.ts
+++ web/src/app/simple/campaign/[id]/farcaster-image/route.ts
@@ -1,4 +1,5 @@
-import detailImageGenerator from "@/components/detailImageGenerator";
+import React from "react";
+import { ImageResponse } from "next/og";
 
 const size = {
   width: 1200,
@@ -7,5 +8,16 @@ const size = {
 type Params = Promise<{ id: string }>;
 export async function GET(_: Request, { params }: { params: Params }) {
   const { id } = await params;
-  return await detailImageGenerator(id, size);
+  return new ImageResponse(
+    React.createElement("img", {
+      src: `https://9lives.so/dppm-og-image.png`,
+      alt: "9lives.so",
+      width: size.width,
+      height: size.height,
+    }),
+    {
+      width: size.width,
+      height: size.height,
+    },
+  );
 }
--- web/src/app/simple/campaign/[id]/opengraph-image.tsx
+++ web/src/app/simple/campaign/[id]/opengraph-image.tsx
@@ -1,4 +1,5 @@
 import detailImageGenerator from "@/components/detailImageGenerator";
+import { ImageResponse } from "next/og";
 
 export const size = {
   width: 1200,
@@ -8,5 +9,14 @@ export const contentType = "image/png";
 type Params = Promise<{ id: string }>;
 export default async function ImageOG({ params }: { params: Params }) {
   const { id } = await params;
-  return await detailImageGenerator(id, size);
+  return new ImageResponse(
+    (
+      <img
+        src={`https://9lives.so/dppm-og-image.png`}
+        alt="9lives.so"
+        width={size.width}
+        height={size.height}
+      />
+    ),
+  );
 }
