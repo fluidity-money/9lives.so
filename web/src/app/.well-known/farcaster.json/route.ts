7441a508ad4e21597d0af2191282c20ec6f0e896
bayge <alex@fluidity.money>
2025-11-24 12:32:49 -0300

    Update farcaster route

--- web/src/app/.well-known/farcaster.json/route.ts
+++ web/src/app/.well-known/farcaster.json/route.ts
@@ -9,6 +9,9 @@ export async function GET() {
       signature:
         "MHhhYzkwYjg2OWQyMDFkYzY1OTQ4MTNlYTZjNDQ1YzAwZjM3NzU2ZDg5N2JmZjcxY2MyMzE2MTFiNDI0N2YwZDE0MGM1Y2E1YWExODIzOWY5Y2QyOTljY2U2ZWYxNTcwODdjMTQ1MjBiN2ZlNzY1YjdjYmYyNDE5MThlMWMzNDQzYTFi",
     },
+    baseBuilder: {
+      ownerAddress: "0x60143A0b1DA42FfEc63ADDB9D05A43485275Ede1"
+    },
     frame: appConfig.frame,
   };
 
