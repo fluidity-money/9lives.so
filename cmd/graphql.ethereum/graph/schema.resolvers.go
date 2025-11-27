50d2d41ddefcd1dd95c0052735fdcb5aeab0a3d0
ogous <oguzxan.ali@gmail.com>
2025-11-27 13:01:28 +0300

    fix query typo

--- cmd/graphql.ethereum/graph/schema.resolvers.go
+++ cmd/graphql.ethereum/graph/schema.resolvers.go
@@ -1968,7 +1968,7 @@ func (r *queryResolver) UnclaimedCampaigns(ctx context.Context, address string, 
 	where
     py.spender = ?
     and py.was_spent = false
-	and bs_sum.campaign_content->>'ending')::numeric < EXTRACT(EPOCH FROM NOW())
+	and (bs_sum.campaign_content->>'ending')::numeric < EXTRACT(EPOCH FROM NOW())
 	`
 	if token != nil {
 		query += ` and bs_sum.campaign_content->'priceMetadata'->>'baseAsset' = ?`
