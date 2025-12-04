8729817306ec54fb1233d2a83d670838d5cc254b
ogous <oguzxan.ali@gmail.com>
2025-12-04 15:34:46 +0300

    add 9lives points component

--- web/src/components/pointsMenuButton.tsx
+++ web/src/components/pointsMenuButton.tsx
@@ -1,9 +1,16 @@
+"use client";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import Image from "next/image";
 import RightCaretIcon from "#/icons/right-caret.svg";
 import RetroCard from "./cardRetro";
+import use9LivesPoints from "@/hooks/use9LivesPoints";
+import Button from "./themed/button";
+import Link from "next/link";
+import LoadingImage from "#/icons/loading.svg";
+import { combineClass } from "@/utils/combineClass";
 
 export default function PointMenuButton() {
+  const { data, isLoading } = use9LivesPoints();
   return (
     <Menu>
       <MenuButton className="group flex items-center">
@@ -19,8 +26,53 @@ export default function PointMenuButton() {
       </MenuButton>
       <MenuItems anchor="bottom" className="z-[99]">
         <MenuItem>
-          <RetroCard title="Coming Soon" position="middle" showClose={false}>
-            <p className="font-chicago">🥳 9lives points are coming soon! 🎉</p>
+          <RetroCard
+            title="9lives Points"
+            className="flex flex-col gap-4"
+            position="middle"
+            showClose={false}
+          >
+            <p className="text-center font-geneva uppercase">
+              My Accumulated Points
+            </p>
+            <span
+              className={combineClass(
+                isLoading ? "bg-[#ccc]" : "bg-9green",
+                "mx-auto p-2 font-chicago text-2xl",
+              )}
+            >
+              {isLoading ? (
+                <Image
+                  src={LoadingImage}
+                  className="animate-spin"
+                  alt="Loading"
+                  width={24}
+                />
+              ) : (
+                data
+              )}
+            </span>
+            <Link
+              target="_blank"
+              href="https://medium.com/@Superpositionso"
+              className="mx-auto font-geneva text-xs underline"
+            >
+              Learn More
+            </Link>
+            <div className="flex flex-col gap-1">
+              <Link
+                href="https://superposition.so/mainnet-dapps"
+                target="_blank"
+              >
+                <Button
+                  title="Get More Points"
+                  size={"large"}
+                  intent={"cta"}
+                  className={"w-full"}
+                />
+              </Link>
+              <Button title="Coming Soon" disabled className={"w-full"} />
+            </div>
           </RetroCard>
         </MenuItem>
       </MenuItems>
