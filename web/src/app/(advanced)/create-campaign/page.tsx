6bd9c261c2985915dea2fc84aef63d0982e298b8
ogous <oguzxan.ali@gmail.com>
2025-12-10 17:39:37 +0300

    add points indicator to campaign create

--- web/src/app/(advanced)/create-campaign/page.tsx
+++ web/src/app/(advanced)/create-campaign/page.tsx
@@ -19,11 +19,16 @@ export default async function CreateCampaign() {
         <div className="flex items-center gap-2">
           <h2 className="font-chicago text-2xl">Create Campaign</h2>
         </div>
-        <p className="text-center text-xs">
+        <p className="text-center text-sm">
           Create a campaign for other people to participate in!
           <br />
-          <strong>Earn fees by creating markets:</strong> collect 1% of all buy
-          volume as the creator, and 1% as a LP. 1% goes to the DAO.
+          <br />
+          <span className="bg-9green px-1 py-0.5 font-bold">
+            Earn fees and points by creating markets
+          </span>
+          <br />
+          Collect 1% of all buy volume as the creator, 1% as a LP and win 2
+          9lives points. 1% goes to the DAO.
         </p>
       </div>
       <div className="flex flex-1 flex-col gap-7 md:flex-row">
