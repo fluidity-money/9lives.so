ba579c67636e1d63335381ac224fedb491cb5e54
ogous <oguzxan.ali@gmail.com>
2025-12-10 15:13:04 +0300

    disable leaderboard categories

--- web/src/hooks/useLeaderBoardCategories.tsx
+++ web/src/hooks/useLeaderBoardCategories.tsx
@@ -1,30 +1,29 @@
-import { requestLeaderboardCategories } from "@/providers/graphqlClient";
-import { Leader } from "@/types";
-import formatFusdc from "@/utils/format/formatUsdc";
-import { useQuery } from "@tanstack/react-query";
+// import { requestLeaderboardCategories } from "@/providers/graphqlClient";
+// import { Leader } from "@/types";
+// import formatFusdc from "@/utils/format/formatUsdc";
+// import { useQuery } from "@tanstack/react-query";
 
-export default function useLeaderBoardCategories() {
-  return useQuery<{
-    referrers: Leader[];
-    volume: Leader[];
-    creators: Leader[];
-  }>({
-    queryKey: ["leaderboard-categories"],
-    queryFn: async () => {
-      const res = await requestLeaderboardCategories();
-      const mapper = (arr: { address: string; volume: string }[]) =>
-        arr.map(
-          (i, idx) =>
-            ({
-              wallet: i.address,
-              ranking: idx + 1,
-              scoring: +formatFusdc(i.volume, 2),
-            }) as Leader,
-        );
-      const referrers = mapper(res.referrers);
-      const creators = mapper(res.creators);
-      const volume = mapper(res.volume);
-      return { referrers, creators, volume };
-    },
-  });
-}
+// export default function useLeaderBoardCategories() {
+//   return useQuery<{
+//     referrers: Leader[];
+//     volume: Leader[];
+//     creators: Leader[];
+//   }>({
+//     queryKey: ["leaderboard-categories"],
+//     queryFn: async () => {
+//       const res = await requestLeaderboardCategories();
+//       const mapper = (arr: { address: string; volume: string }[]) =>
+//         arr.map(
+//           (i, idx) =>
+//             ({
+//               wallet: i.address,
+//               amount: +formatFusdc(i.volume, 2),
+//             }) as Leader,
+//         );
+//       const referrers = mapper(res.referrers);
+//       const creators = mapper(res.creators);
+//       const volume = mapper(res.volume);
+//       return { referrers, creators, volume };
+//     },
+//   });
+// }
