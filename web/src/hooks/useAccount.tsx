32f4fd077914cfef02f0c8c97f206de72d79572c
ogous <oguzxan.ali@gmail.com>
2025-12-16 11:07:03 +0300

    add storing secret logic

--- web/src/hooks/useAccount.tsx
+++ web/src/hooks/useAccount.tsx
@@ -14,6 +14,17 @@ function encodeNonceBE(nonce: number): string {
   view.setUint32(0, nonce, false);
   return Buffer.from(buf).toString("hex");
 }
+function storeSecret(secret: string) {
+  const ONE_MONTH = 1000 * 60 * 60 * 24 * 30;
+  const FIVE_MINS = 1000 * 60 * 5;
+  window.localStorage.setItem(
+    "9lives-account-secret",
+    JSON.stringify({
+      secret,
+      expireAt: new Date(Date.now() + ONE_MONTH - FIVE_MINS).toUTCString(),
+    }),
+  );
+}
 
 export default function useAccount() {
   const account = useActiveAccount();
@@ -22,12 +33,16 @@ export default function useAccount() {
     const publicKey = await requestPublicKey();
     const signature = await account?.signMessage({ message: publicKey });
     const { r, s, v } = Signature.from(signature);
-    return await createAccount({
+    const response = await createAccount({
       eoaAddr: account.address.slice(2),
       r: r.slice(2),
       s: s.slice(2),
       v,
     });
+    if (!response) {
+      throw new Error("Account creation failed");
+    }
+    storeSecret(response.secret);
   };
   const getSecret = async () => {
     if (!account) throw new Error("No wallet is connected");
@@ -37,13 +52,17 @@ export default function useAccount() {
     const message = publicKey + nonceHex;
     const signature = await account?.signMessage({ message });
     const { r, s, v } = Signature.from(signature);
-    return await requestSecret({
+    const secret = await requestSecret({
       eoaAddr: account.address.slice(2),
       nonce,
       s: s.slice(2),
       r: r.slice(2),
       v,
     });
+    if (!secret) {
+      throw new Error("Account secret is not retreived");
+    }
+    storeSecret(secret);
   };
   const isCreated = async () => {
     if (!account) throw new Error("No wallet is connected");
