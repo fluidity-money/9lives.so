d1a348309e44ed35e3c97af5dec70ebe1c708311
ogous <oguzxan.ali@gmail.com>
2025-12-22 12:30:45 +0300

    add paxg 15min market

--- web/src/config/app.ts
+++ web/src/config/app.ts
@@ -108,7 +108,7 @@ const simpleMarkets = {
     tabTitle: "GOLD",
     openDays: [0, 1, 2, 3, 4, 5, 6],
     decimals: 2,
-    periods: ["hourly"],
+    periods: ["hourly", "15mins"],
     openHours: ["00:00", "23:59"],
     tz: "UTC",
   },
