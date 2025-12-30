97e391c81c42fe73969c808d5e5e7665417fb1d6
ogous <oguzxan.ali@gmail.com>
2025-12-30 12:33:55 +0300

    fix graphql main.go

--- cmd/graphql.ethereum/main.go
+++ cmd/graphql.ethereum/main.go
@@ -16,6 +16,9 @@ import (
 	"github.com/fluidity-money/9lives.so/cmd/graphql.ethereum/graph"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 
 	ethCommon "github.com/ethereum/go-ethereum/common"
@@ -145,16 +148,16 @@ func main() {
 			AdminSecret:             adminSecret,
 			PaymasterMinimumUSDCGas: paymasterMinFee,
 			PaymasterSenderAddr:     ethCommon.HexToAddress(paymasterSenderAddr),
-			PriceResolverAddr: ethCommon.HexToAddress(config.PriceResolverAddress),
+			PriceResolverAddr:       ethCommon.HexToAddress(config.PriceResolverAddress),
 		},
 	}))
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
 	srv.AddTransport(transport.POST{})
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	srv.SetQueryCache(lru.New(1000))
 	srv.Use(extension.Introspection{})
 	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
+		Cache: lru.New(100),
 	})
 	http.Handle("/", corsMiddleware{srv})
 	http.Handle("/playground", playground.Handler("9lives.so playground", "/"))
