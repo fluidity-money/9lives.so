841f46c32349bc83c39699e2fa7aed9e6969d28d
ogous <oguzxan.ali@gmail.com>
2026-01-08 10:58:27 +0300

    add fifteen minute market calculation

--- web/src/hooks/usePointsForDppmMint.tsx
+++ web/src/hooks/usePointsForDppmMint.tsx
@@ -1,13 +1,34 @@
-const hourUnit = 60 * 60 * 1000;
+const minuteUnit = 60 * 1000;
+const fifteenMinUnit = 15 * minuteUnit;
+const hourUnit = 60 * minuteUnit;
 const dayUnit = hourUnit * 24;
+
+const fifteenMinMltplr = 2;
 const hourMltplr = 2;
 const dayMltplr = 4;
+
 const minDecay = 0.1;
+
 export default function usePointsForDppmMint(starting: number, ending: number) {
-  const isDailyMarket = ending - starting >= 1000 * 60 * 60 * 24;
-  const unit = isDailyMarket ? dayUnit : hourUnit;
-  const mltplr = isDailyMarket ? dayMltplr : hourMltplr;
+  const duration = ending - starting;
+
+  const isFifteenMinMarket = duration <= fifteenMinUnit;
+  const isDailyMarket = duration >= dayUnit;
+
+  const unit = isFifteenMinMarket
+    ? fifteenMinUnit
+    : isDailyMarket
+      ? dayUnit
+      : hourUnit;
+
+  const mltplr = isFifteenMinMarket
+    ? fifteenMinMltplr
+    : isDailyMarket
+      ? dayMltplr
+      : hourMltplr;
+
   const diffMs = Date.now() - new Date(starting).getTime();
   const fractionOfTime = diffMs / unit;
+
   return Math.max(mltplr - fractionOfTime * (mltplr - minDecay), minDecay);
 }
