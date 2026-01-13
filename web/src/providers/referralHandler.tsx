26c7e3047d2b42094a2019150f6551079c0f875c
ogous <oguzxan.ali@gmail.com>
2026-01-13 09:58:35 +0300

    remove derived state

--- web/src/providers/referralHandler.tsx
+++ web/src/providers/referralHandler.tsx
@@ -1,26 +1,12 @@
 import RefereeDialog from "@/components/referral/refereeDialog";
 import Modal from "@/components/themed/modal";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function ReferralHandler() {
   const searchParams = useSearchParams();
   const code = searchParams.get("referral");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (code) {
-      setIsModalOpen(true);
-    } else {
-      setIsModalOpen(false);
-    }
-  }, [code]);
-
-  useEffect(() => {
-    if (isModalOpen) {
-      setIsModalOpen(true);
-    }
-  }, [isModalOpen]);
+  const [isModalOpen, setIsModalOpen] = useState(() => Boolean(code));
 
   if (!code) return null;
 
