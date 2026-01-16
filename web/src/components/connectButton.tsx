d41862a53f7f51b54f092a50893ac130bc1d45ef
ogous <oguzxan.ali@gmail.com>
2026-01-16 16:40:21 +0300

    customize connect button

--- web/src/components/connectButton.tsx
+++ web/src/components/connectButton.tsx
@@ -1,7 +1,44 @@
 "use client";
-// import { useEffect, useState } from "react";
-import { AppKitButton } from "@reown/appkit/react";
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
+import makeBlockie from "ethereum-blockies-base64";
+import Image from "next/image";
+import LoginIcon from "#/icons/login.svg";
+import useFeatureFlag from "@/hooks/useFeatureFlag";
+import { combineClass } from "@/utils/combineClass";
 
 export default function ConnectButton() {
-  return <AppKitButton />;
+  const account = useAppKitAccount();
+  const enableCreate = useFeatureFlag("enable campaign create");
+  const { open } = useAppKit();
+  return (
+    <div
+      onClick={() => open()}
+      className={combineClass(
+        enableCreate ? "border-l-2" : "border-0",
+        "flex h-10 cursor-pointer items-center border-l-black px-2",
+      )}
+    >
+      {account.isConnected && account.address ? (
+        <div className="flex items-center gap-1">
+          <Image
+            alt={""}
+            width={24}
+            height={24}
+            className="size-6 shrink-0 border border-9black bg-9gray"
+            src={makeBlockie(account.address)}
+          />
+          <div>
+            <span className="font-chicago text-xs font-bold">
+              {account.address.slice(0, 4)}...{account.address.slice(-4)}
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center gap-1">
+          <span className="font-chicago text-xs underline">Connect Wallet</span>{" "}
+          <Image src={LoginIcon} height={14} width={14} alt="Connect Wallet" />
+        </div>
+      )}
+    </div>
+  );
 }
