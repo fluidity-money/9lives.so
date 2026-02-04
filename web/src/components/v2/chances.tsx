11a1cb923c8da210c5c3c2a3fa1b695300fea091
ogous <oguzxan.ali@gmail.com>
2026-02-04 17:30:11 +0300

    update chart

--- web/src/components/v2/chances.tsx
+++ web/src/components/v2/chances.tsx
@@ -10,12 +10,12 @@ export default function SimpleChance({
   isLoading: boolean;
 }) {
   return (
-    <div className="mb-4 flex flex-col gap-1">
+    <div className="mb-2 flex flex-col gap-1">
       {isLoading ? (
         <div className="skeleton h-[30px] w-full" />
       ) : (
         <div className="relative flex flex-row items-center gap-0.5">
-          <span className="absolute left-1.5 top-[3px] text-sm font-bold text-green-700">
+          <span className="absolute left-1.5 top-1 text-sm font-bold text-green-700">
             {yes}%
           </span>
           <div
@@ -26,7 +26,7 @@ export default function SimpleChance({
             className="flex h-[30px] rounded-md bg-red-300"
             style={{ flex: no }}
           />
-          <span className="absolute right-1.5 top-[3px] text-sm font-bold text-red-700">
+          <span className="absolute right-1.5 top-1 text-sm font-bold text-red-700">
             {no}%
           </span>
         </div>
