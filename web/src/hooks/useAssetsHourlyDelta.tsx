dc2b1b581b00c207562f8691f47232de70583247
ogous <oguzxan.ali@gmail.com>
2026-02-25 14:55:13 +0300

    format return type of hook

--- web/src/hooks/useAssetsHourlyDelta.tsx
+++ web/src/hooks/useAssetsHourlyDelta.tsx
@@ -1,11 +1,15 @@
 import { requestAssetsHourlyDelta } from "@/providers/graphqlClient";
+import { AssetMetadata } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useAssetsHourlyDelta() {
-  return useQuery({
+  return useQuery<(AssetMetadata | null)[], Error, AssetMetadata[]>({
     queryKey: ["assetsHourlyDelta"],
     queryFn: async () => {
       return await requestAssetsHourlyDelta();
+    },
+    select(data) {
+      return data.filter((i) => !!i);
     },
   });
 }
