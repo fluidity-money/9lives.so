d98556b35249e9d91a311780cb6e53ed5fc39c87
ogous <oguzxan.ali@gmail.com>
2026-02-27 18:38:23 +0300

    improve keyboard text

--- web/src/components/v2/keyboardCommandAlert.tsx
+++ web/src/components/v2/keyboardCommandAlert.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import CloseButton from "./closeButton";
 
 export default function KeyboardCommandAlert() {
-  const charStyle = "p-1 rounded-lg bg-neutral-200 text-2black";
+  const charStyle = "px-1 py-0.5 rounded-lg bg-neutral-200 text-2black";
 
   const [dismissed, setDismissed] = useState(false);
 
@@ -16,13 +16,13 @@ export default function KeyboardCommandAlert() {
       <CloseButton close={() => setDismissed(true)} />
       <h6 className="mb-4">Use Keyboard Actions</h6>
       <p className="mb-4 text-center text-xs leading-6">
-        You can use quick keyboard actions to nagivate between assets, and give
-        commands to buy. Use <span className={charStyle}>⌘</span> +{" "}
+        You can use quick keyboard shortcuts to navigate between assets and
+        issue buy commands. Use <span className={charStyle}>⌘</span> +{" "}
         <span className={charStyle}>←</span> and{" "}
-        <span className={charStyle}>→</span> to navigate,{" "}
+        <span className={charStyle}>→</span> to move between assets,{" "}
         <span className={charStyle}>↑</span> and{" "}
-        <span className={charStyle}>↓</span> to predict if price goes up or
-        down.
+        <span className={charStyle}>↓</span> to predict whether the price will
+        go up or down.
       </p>
       <Button
         title="Dismiss"
