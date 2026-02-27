96fa2e2f387fb4eea9b5eae6a88591c7e43fc083
ogous <oguzxan.ali@gmail.com>
2026-02-27 17:39:37 +0300

    add navigation button styles

--- web/src/components/v2/body.tsx
+++ web/src/components/v2/body.tsx
@@ -20,6 +20,21 @@ import useAssetsHourlyDelta from "@/hooks/useAssetsHourlyDelta";
 import ArrowIcon from "../arrowIcon";
 import { getNextCampaign, getPrevCampaign } from "@/utils/getCampaign";
 import useAssets from "@/hooks/useAssets";
+import { combineClass } from "@/utils/combineClass";
+
+const RightCarot = ({ className = "size-6" }: { className?: string }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.26186 19.6922L16.2161 12.738C16.3133 12.6411 16.3905 12.5259 16.4431 12.3992C16.4958 12.2724 16.5229 12.1364 16.5229 11.9991C16.5229 11.8618 16.4958 11.7259 16.4431 11.5991C16.3905 11.4723 16.3133 11.3572 16.2161 11.2602L9.26186 4.30606C9.0659 4.11009 8.80012 4 8.52298 4C8.24585 4 7.98006 4.11009 7.7841 4.30606C7.58813 4.50202 7.47804 4.7678 7.47804 5.04494C7.47804 5.32207 7.58813 5.58786 7.7841 5.78382L14.0003 12L7.78323 18.2162C7.58726 18.4121 7.47717 18.6779 7.47717 18.9551C7.47717 19.2322 7.58726 19.498 7.78323 19.6939C7.97919 19.8899 8.24498 20 8.52211 20C8.79925 20 9.06503 19.8899 9.26099 19.6939L9.26186 19.6922Z"
+      fill="#181818"
+    />
+  </svg>
+);
 
 export default function SimpleBody({
   initialAssets,
@@ -72,9 +87,10 @@ export default function SimpleBody({
     setSymbol(prev.symbol);
     setPeriod(prev.period);
   };
+  const buttonStyle = `md:flex absolute inset-y-0 px-4 items-center justify-center hover:bg-neutral-200 bg-neutral-100 rounded-xl cursor-pointer hidden`;
   return (
     <>
-      <div className="rounded-xl border border-neutral-400 bg-2white p-4">
+      <div className="relative rounded-xl border border-neutral-400 bg-2white p-4">
         <div className="mb-2 flex items-center justify-between">
           <div className="flex gap-2">
             <Image
@@ -112,9 +128,19 @@ export default function SimpleBody({
           openModal={() => setIsBuyDialogOpen(true)}
           data={data}
         />
+        <div
+          onClick={handlePrevCamp}
+          className={combineClass(buttonStyle, "left-[-72px]")}
+        >
+          <RightCarot className="size-6 rotate-180" />
+        </div>
+        <div
+          onClick={handleNextCamp}
+          className={combineClass(buttonStyle, "right-[-72px]")}
+        >
+          <RightCarot />
+        </div>
       </div>
-      <button onClick={handleNextCamp}>Go to next</button>
-      <button onClick={handlePrevCamp}>Go to prev</button>
       <div className="sticky inset-x-0 bottom-0 z-20 flex flex-col gap-2 bg-2white md:static md:flex-row md:bg-transparent md:p-0">
         <SimpleButtons
           yes={yesChance}
