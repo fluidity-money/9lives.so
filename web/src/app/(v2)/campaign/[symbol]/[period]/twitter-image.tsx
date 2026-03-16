f990e466b6b59b2e2909bf59ca6a6167fbb48327
ogous <oguzxan.ali@gmail.com>
2026-03-16 14:43:17 +0300

    update og images

--- web/src/app/(v2)/campaign/[symbol]/[period]/twitter-image.tsx
+++ web/src/app/(v2)/campaign/[symbol]/[period]/twitter-image.tsx
@@ -1,14 +1,23 @@
-import Image from "next/image";
 import { ImageResponse } from "next/og";
 
 export const size = {
   width: 1200,
   height: 630,
 };
-export const contentType = "image/png";
-export default async function ImageOG() {
+
+export function generateImageMetadata() {
+  return [
+    {
+      contentType: "image/png",
+      id: "default",
+      size,
+    },
+  ];
+}
+
+export default async function Icon() {
   return new ImageResponse(
-    <Image
+    <img
       src={"https://9lives.so/images/dppm-og-image.png"}
       alt="9lives.so"
       width={size.width}
