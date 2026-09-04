a8863dbbe67ea5254d33bf6fd48c1419db27c21c
bayge <alex@fluidity.money>
2026-09-04 14:49:27 +0800

    Add missing rfqhub event fields

--- db/migrations/1785727007-initial_rfqhub.sql
+++ db/migrations/1785727007-initial_rfqhub.sql
@@ -5,6 +5,8 @@ CREATE TABLE rfqhub_events_balance_changed (
 	created_by TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	block_hash HASH NOT NULL,
 	transaction_hash HASH NOT NULL,
+	block_number INTEGER NOT NULL,
+	emitter_addr ADDRESS NOT NULL,
 
 	account_addr ADDRESS NOT NULL,
 	asset ADDRESS NOT NULL,
